094327e015f387de9b3bb8805fdbf6e9cf497ef4
Capt Greg <CaptGreg@github.com>
2015-05-18 20:06:24 -0400

    tidy

---
+++ exception.cpp
@@ -0,0 +1,28 @@
+ 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int sub(int argc, char**argv)
+{
+  try {
+
+    if(argc > 1)throw string("argc > 1");
+
+    else throw "argc not > 1";
+
+  } catch(const string& e){
+
+    cout << "sub: threw a string: " << e << endl;
+
+  } catch(const char* e){
+
+    cout << "sub: threw a char*: " << e << endl;
+
+  }
+}
+
+int main(int argc, char**argv)
+{
+   sub(argc, argv);
+}
