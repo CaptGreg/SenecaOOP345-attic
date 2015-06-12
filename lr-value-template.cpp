ebc8ef4b77a222f8687510cedb201aaf6e81ef14
CaptGreg <john.blair@senecacollege.ca>
2015-06-12 11:36:42 -0400

    fiddle with lr-value-template.cpp

--- lr-value-template.cpp
+++ lr-value-template.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <functional> // std::bind std::function
 using namespace std;
 
 template <typename T>
-void print(T&  i) { cout << i << " <-- is an L-value\n"; }
+void print(T&  i) { cout << i << " <-- is an L-value (function template)\n"; }
 template <typename T>
-void print(T&& i) { cout << i << " <-- is an R-value\n"; }
+void print(T&& i) { cout << i << " <-- is an R-value (function template)\n"; }
+
+// can we override a function template that happens to be overloaded (same name)?
+// NO!    :-(
+// template <>
+// void print(int&  i) { cout << i << " <-- is an L-value (function template override - specializion)\n"; }
+// template <>
+// void print(int&& i) { cout << i << " <-- is an R-value (function template override - specialization)\n"; }
 
 int main()
 {
@@ -41,14 +49,21 @@ int main()
 
   // try a lambda function
   auto l = [] () { return 5; };
-  P(l);              // interesting, prints 1, wonder why?
-  P(std::move(l));   // interesting, prints 1, wonder why?
+  P(l);              // interesting, why does it compile? prints 1, wonder why?
+  P(std::move(l));   // interesting, why does it compile? prints 1, wonder why?
   P(l());            // call the lambda function
   P(std::move(l())); // call the lambda function
 
+  int (*n)()             = [] () { return 3; };  // C 1977 style pointer, C++11 lambda
+  P(n);             // no error
+  P(n());
+  std::function<int()> o = [] () { return 3; };   // C++ style pointer, C++11 lambda, syntax error
+  // P(o);             // syntax error
+  P(o());
+
   // try a function object
-  class X {public: int operator() () { return 77; } };
-  X x;
-  P(x());            // call the function object
-  P(std::move(x())); // call the function object
+  class MFO {public: int operator() () { return 77; } };
+  MFO mfo;
+  P(mfo());            // call the function object, returns 99, which is a r-value
+  P(std::move(mfo())); // call the function object, returns 99, which is a r-value
 }
