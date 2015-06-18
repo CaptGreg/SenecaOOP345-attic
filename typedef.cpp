1b4d013994f588dafb2d0d2e01ad6fdd76b3c986
CaptGreg <john.blair@senecacollege.ca>
2015-06-18 14:21:54 -0400

    fix comment in ll9.cpp, explore typeid(var).name() in typedef.cpp

--- typedef.cpp
+++ typedef.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+
+#include <typeinfo>
+
+// a student thought typedef(var).name() returned whether 'var' was a l or r value.
+// does it?
+// NO - syntax error (as expected)
+// but does typeid(var).name() return r or l value?
+// NO!
+template<typename T>
+// void print(T& i) { cout << typedef(i).name() << "\n"; }
+void print(T& i)  { cout << i << " is an l-value, typeid(t).name() returns: " << typeid(i).name() << "\n"; }
+
+template<typename T>
+// void print(T&& i) { cout << typedef(i).name() << "\n"; }
+void print(T&& i) { cout << i << " is an r-value, typeid(t).name() returns: " << typeid(i).name() << "\n"; }
+
 int main(int argc, char**argv)
 {
 #ifdef DEBUG
@@ -13,7 +29,11 @@ typedef class XXX { };
 
   REAL x = 9.0;
 
-  cout << " sizeof(REAL) = " << sizeof(REAL) << endl;
+  cout << " sizeof(REAL) = " << sizeof(REAL) << "\n";
 
-  cout << x << endl;
+  cout << x << "\n";
+  
+  print(x);
+
+  print(move(x));
 }
