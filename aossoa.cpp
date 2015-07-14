8accb907cb3b66e0342aebded241b63152571843
Capt Greg <CaptGreg@github.com>
2015-07-14 15:25:33 -0400

    diy shared_ptr sp8.cpp, tweak sp7.cpp aossoa.cpp

--- aossoa.cpp
+++ aossoa.cpp
@@ -2,34 +2,12 @@
 #include <chrono>
 using namespace std;
 
+// AoS : Array of Structures
+// SoA : Structure of Arrays
+
 // AoS vs SoA are hot topics.  GPUs prefer AoS.
 
 // Let's measure which is faster.
-
-const int N = 100*1000*1000;
-struct {
-    uint8_t r, g, b;
-} AoS[N];
-
-struct {
-    uint8_t r[N];
-    uint8_t g[N];
-    uint8_t b[N];
-} SoA;
-
-#if 0
-http://stackoverflow.com/questions/8377667/layout-in-memory-of-a-struct-struct-of-arrays-and-array-of-structs-in-c-c/8377717#8377717
-
-These have the following trade-offs:
-
-    AoS tends to be more readable to the programmer as each "object" is kept together.
-    AoS may have better cache locality if all the members of the struct are accessed together.
-    SoA could potentially be more efficient since grouping same datatypes together sometimes exposes vectorization.
-    In many cases SoA uses less memory because padding is only between arrays rather than between every struct.
-#endif
-
-int main()
-{
   class Timer { // C++11 chrono stop-watch timer class. Needs "#include <chrono>".
     chrono::time_point<chrono::high_resolution_clock> start;
     chrono::time_point<chrono::high_resolution_clock> stop;
@@ -44,18 +22,27 @@ int main()
    }
   };
 
+const int N = 100*1000*1000+1;
+struct {
+    uint8_t r, g, b;
+} AoS[N];
+
+struct {
+    uint8_t r[N];
+    uint8_t g[N];
+    uint8_t b[N];
+} SoA;
+
+
+int main()
+{
   Timer t;
+
+  cout << "\n";
+
   #define P(X) cout << "sizeof(" << #X << ")=" << sizeof(X) << "\n";
 
-  P(SoA)
-  t.Start();
-  for(int i = 0 ; i < N; i++) {
-     SoA.r[i] = SoA.g[i] = SoA.b[i] = i; 
-  }
-  t.Stop();
-  uint64_t tSoA = t.nsecs();
-  cout << "SoA " << tSoA << " ns\n";
-
+  cout << "struct { uint8_t r, g, b; } AoS[N];\n";
   P(AoS)
   t.Start();
   for(int i = 0 ; i < N; i++) {
@@ -65,5 +52,21 @@ int main()
   uint64_t tAoS = t.nsecs();
   cout << "AoS " << tAoS << " ns\n";
 
+  cout << "\n";
+
+  cout << "struct { uint8_t r[N]; uint8_t g[N]; uint8_t b[N]; } SoA;\n";
+  P(SoA)
+  t.Start();
+  for(int i = 0 ; i < N; i++) {
+     SoA.r[i] = SoA.g[i] = SoA.b[i] = i; 
+  }
+  t.Stop();
+  uint64_t tSoA = t.nsecs();
+  cout << "SoA " << tSoA << " ns\n";
+
+  cout << "\n";
+
   cout << "Ratio AoS/SoA = " << double(tAoS) / double(tSoA) << "\n";
+
+  cout << "\n";
 }
