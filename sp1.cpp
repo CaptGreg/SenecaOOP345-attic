c68ff70a5c9c191e410d15a85de5221742fd4a9c
John (Greg)ory Blair <john.blair@senecacollege.ca>
2015-07-14 22:13:01 -0400

    sp[0-2].cpp add atexit function

--- sp1.cpp
+++ sp1.cpp
@@ -7,14 +7,15 @@ string name;
 public:
   X(const std::string& n) { name = n; cout << "X ctor " << name << "\n"; }
   ~X() { cout << "X dtor " << name << "\n"; }
-  void hello() { cout << "hello from " << name << "\n\n"; }
+  void hello() const { cout << "hello from " << name << "\n\n"; }
 };
 
-X xgbefore("global xgbefore before main");
+static X xgbefore("global xgbefore before main");
 
 int main(int argc, char**argv)
 {
   cout << "main starting...\n";
+  atexit( [] () { cout << "atexit function\n"; } );
   X x(std::string("main x"));
   X xx("main xx");
 
