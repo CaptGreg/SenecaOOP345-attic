0767c0e878a3f620c5562b59c28470a211b89eff
Capt Greg <CaptGreg@github.com>
2015-07-15 23:41:01 -0400

    letest rev of smart pointer sp[0-9].cpp

--- sp1.cpp
+++ sp1.cpp
@@ -3,19 +3,20 @@
 using namespace std;
 
 class X {
-string name;
+  string name;
+  uint8_t memBlock[1024*1024];  // 1 MByte
 public:
   X(const std::string& n) { name = n; cout << "X ctor " << name << "\n"; }
   ~X() { cout << "X dtor " << name << "\n"; }
   void hello() const { cout << "hello from " << name << "\n\n"; }
 };
 
-static X xgbefore("global xgbefore before main");
+X xgbefore("global xgbefore before main");
 
 int main(int argc, char**argv)
 {
   cout << "main starting...\n";
-  atexit( [] () { cout << "atexit function\n"; } );
+  // atexit( [] () { cout << "atexit function\n"; } );
   X x(std::string("main x"));
   X xx("main xx");
 
