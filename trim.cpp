377dc590e88b39660b26e28fa48fa24a5b0e0abc
CaptGreg <j.greg.blair@gmail.com>
2015-11-17 23:14:34 -0500

    string trim

---
+++ trim.cpp
@@ -0,0 +1,74 @@
+// http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+
+#include <algorithm> 
+#include <functional> 
+#include <cctype>
+#include <locale>
+
+// trim from start
+std::string &ltrim(std::string &s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+  return s;
+}
+
+// trim from end
+std::string &rtrim(std::string &s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+  return s;
+}
+
+// trim from both ends
+std::string &trim(std::string &s) // GB works!
+{
+  return ltrim(rtrim(s));
+}
+
+
+// http://www.toptip.ca/2010/03/trim-leading-or-trailing-white-spaces.html
+
+// Note LTrim() and RTrim() return references. They are destructive. 
+std::string &LTrim(std::string &s)
+{
+  // s.erase(s.begin(),find_if_not(s.begin(),s.end(),[](int c){return isspace(c);}));
+  auto f = [] (int c) { return isspace(c); }; // GB break out lambda
+  s.erase(s.begin(), find_if_not(s.begin(),s.end(), f) ); // GB forward iterators
+  return s;
+}
+
+std::string &RTrim(std::string &s)
+{ 
+  // s.erase(find_if_not(s.rbegin(),s.rend(),[](int c){return isspace(c);}).base(), s.end());
+  auto f = [] (int c) { return isspace(c); }; // GB break out lambda
+  s.erase( find_if_not( s.rbegin(), s.rend(), f ).base(), s.end()); // GB reverse iterators
+  return s;
+}
+
+std::string TRim(const std::string &s) // GB works!
+{
+  std::string t=s; // did trim inline, GB convert to returning trimmed result
+  return LTrim(RTrim(t));
+}
+
+
+
+#include <iostream> 
+#include <vector> 
+int main() // GB wrote this test code for the above pair of trim routines.
+{
+   std::vector<std::string> tests = {
+     std::string ( "   a   " ),
+     std::string ( "" ),
+     std::string ( "   " ),
+     std::string ( "\t\t\t\t\t" ),
+     std::string ( "B      " ),
+     std::string ( "   \t C" ),
+   };
+
+   std::cout << "DATA->"; for(auto e : tests) std::cout << ":" << e;  std::cout << ":\n";
+
+   std::cout << "TRim->"; for(auto e : tests) std::cout << ":" << TRim(e);  std::cout << ":\n";
+
+   std::cout << "trim->"; for(auto e : tests) std::cout << ":" << trim(e);  std::cout << ":\n";
+    
+   return 0;
+}
