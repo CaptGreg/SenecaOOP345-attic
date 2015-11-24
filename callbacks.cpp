1b7dcde510eff1f41bef9b0ce24bb6defcf4a6f1
Capt Greg <CaptGreg@github.com>
2015-11-24 15:34:47 -0500

    tidy threading callbacks

--- callbacks.cpp
+++ callbacks.cpp
@@ -12,14 +12,14 @@
 #include <exception>
 using namespace std;
  
-typedef std::function<void ()> Job;
+typedef function<void()> Job; // jobs are void functions without arguments
 
 int main1()
 {
+  cout << "\nmain1\n";
   class JobManager {
     private:
-      vector<Job> jobs;
-   
+      vector<Job> jobs;   
     public:
       void addJob(Job job)
       {
@@ -32,57 +32,56 @@ int main1()
           job();
       }
    
-      void runJobsThreaded(size_t threadsActive) const
+      void runJobsThreaded(size_t maxThreads) const
       {
-        if(jobs.size() <= threadsActive) { // More threads available than jobs.
+        if(jobs.size() <= maxThreads) { // More thread capacity than jobs.
+
+           // Have enough capacity to run all the jobs at once
            thread t[jobs.size()];
-           for(unsigned i = 0; i < jobs.size(); i++)
-             t[i] = thread(jobs[i]);
+           for(unsigned i = 0; i < jobs.size(); i++) // run everything at once
+             t[i] = thread( jobs[i] );
            for(auto& e : t) e.join();
 
         } else {
 
-          thread t[threadsActive];
+          thread t[maxThreads];
           size_t i;
-          for(i = 0; i < threadsActive; i++) { // start batch
+
+          // start up a batch of jobs
+          for(i = 0; i < maxThreads; i++) {
             t[i] = thread(jobs[i]);
           }
 
-          // as a job finishes, start up another job
-          while(i < jobs.size() ) {
-            int oldestThread = (i - threadsActive) % threadsActive;
+          // wait for the oldest job to finish, start up another job
+          for( size_t oldestThread = 0; i < jobs.size() ;  i++, oldestThread++) {
+            if(oldestThread == maxThreads) oldestThread = 0;
 
             t[oldestThread].join();            // wait for oldest job to finish
             t[oldestThread] = thread(jobs[i]); // start new job
-            i++;
           }
 
           // wait for last batch of jobs to finish
-          for(i = jobs.size() - threadsActive; i < jobs.size(); i++) {
-            t[i % threadsActive].join();
-          }
- 
+          for(auto& e : t) e.join();
         }
       }
   };
 
   JobManager jm;
 
-  struct MFO { void operator() (int arg) {cout << string("job ") + to_string(arg) + "\n"; } };
-  MFO mfo;
+  struct MFO { void operator() (int arg) {cout << string("job ") + to_string(arg) + "\n"; } } mfo;
 
-  jm.addJob( [] {cout << "job 0\n"; } );
-  jm.addJob( [] {cout << "job 1\n"; } );
-  jm.addJob( [] {cout << "job 2\n"; } );
+  jm.addJob( [] { cout << "job 0\n"; } );
+  jm.addJob( [] { cout << "job 1\n"; } );
+  jm.addJob( [] { cout << "job 2\n"; } );
   jm.addJob( bind( mfo, 3 ) );
   jm.addJob( bind( mfo, 4 ) );
   jm.addJob( bind( mfo, 5 ) );
-  jm.addJob( bind( [] {cout << "job 6\n"; }) );
-  jm.addJob( bind( [] {cout << "job 7\n"; }) );
-  jm.addJob( bind( [] {cout << "job 8\n"; }) );
-  jm.addJob( bind( [] (int arg) {cout << string("job ") + to_string(arg) + "\n"; }, 9 ) );
-  jm.addJob( bind( [] (int arg) {cout << string("job ") + to_string(arg) + "\n"; }, 10 ) );
-  jm.addJob( bind( [] (int arg) {cout << string("job ") + to_string(arg) + "\n"; }, 11 ) );
+  jm.addJob( bind( [] { cout << "job 6\n"; }) );
+  jm.addJob( bind( [] { cout << "job 7\n"; }) );
+  jm.addJob( bind( [] { cout << "job 8\n"; }) );
+  jm.addJob( bind( [] (int arg) { cout << string("job ") + to_string(arg) + "\n"; }, 9 ) );
+  jm.addJob( bind( [] (int arg) { cout << string("job ") + to_string(arg) + "\n"; }, 10 ) );
+  jm.addJob( bind( [] (int arg) { cout << string("job ") + to_string(arg) + "\n"; }, 11 ) );
 
   // jm.runJobs();
 
@@ -93,6 +92,89 @@ int main1()
 }
 
 /////////////////////////////////////////////////////////////////////////
+int main2()
+{
+  cout << "\nmain2\n";
+  class JobManager {
+    private:
+      vector<Job> jobs;   
+    public:
+      void addJob(Job job)
+      {
+        jobs.push_back(job);
+      }
+   
+      void runJobs() const
+      { // Run jobs sequentially, one after another, one at a time
+        for(auto job : jobs) 
+          job();
+      }
+   
+      void runJobsThreaded(size_t maxThreads) const
+      {
+        if(jobs.size() <= maxThreads) { // More thread capacity than jobs.
+
+           // Have enough thread capacity to run all the jobs at once
+           thread t[jobs.size()];
+           for(unsigned i = 0; i < jobs.size(); i++) // run everything at once
+             t[i] = thread(jobs[i]);
+           for(auto& e : t) e.join();
+
+        } else {
+
+          vector<thread> t; // (maxThreads);
+          size_t i;
+
+          // start up a batch of jobs
+          for(i = 0; i < maxThreads; i++) {
+            t.push_back( thread(jobs[i]) );
+          }
+
+          // run remaining jobs one at a time as each of the oldest jobs finishes
+          for( auto oldest = t.begin(); i < jobs.size() ; i++, oldest++ ) {
+            if(oldest == t.end()) oldest = t.begin();
+
+            oldest->join(); // wait for the oldest job to finish
+            *oldest = thread(jobs[i]); // start new job
+          }
+
+          for(auto& e : t) e.join(); // wait for last batch of jobs to finish
+        }
+      }
+  };
+
+  JobManager jm;
+
+  struct MFO { void operator() (int arg) {cout << string("job ") + to_string(arg) + "\n"; } } mfo;
+
+  auto msSleep = [] (unsigned milliSeconds) { this_thread::sleep_for( std::chrono::milliseconds(milliSeconds) ); };
+  auto nap   = [msSleep] { msSleep(rand() % 1000); };
+
+
+  jm.addJob( [nap] {nap(); cout << "job 0\n"; } );
+  jm.addJob( [nap] {nap(); cout << "job 1\n"; } );
+  jm.addJob( [nap] {nap(); cout << "job 2\n"; } );
+  jm.addJob( bind( mfo, 3 ) );
+  jm.addJob( bind( mfo, 4 ) );
+  jm.addJob( bind( mfo, 5 ) );
+  jm.addJob( bind( [nap] {nap(); cout << "job 6\n"; }) );
+  jm.addJob( bind( [nap] {nap(); cout << "job 7\n"; }) );
+  jm.addJob( bind( [nap] {nap(); cout << "job 8\n"; }) );
+  jm.addJob( bind( [nap] (int arg) {nap(); cout << string("job ") + to_string(arg) + "\n"; }, 9 ) );
+  jm.addJob( bind( [nap] (int arg) {nap(); cout << string("job ") + to_string(arg) + "\n"; }, 10 ) );
+  jm.addJob( bind( [nap] (int arg) {nap(); cout << string("job ") + to_string(arg) + "\n"; }, 11 ) );
+
+  // jm.runJobs();
+
+  // jm.runJobsThreaded(4);
+  jm.runJobsThreaded( std::thread::hardware_concurrency() );
+
+  return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+
 
 #include <functional>
 #include <iostream>
@@ -108,8 +190,9 @@ void execute(const vector<function<void ()>>& fs) {
  
 void plain_old_func() { cout << "I'm an old plain function" << endl; }
  
-int main2()
+int main3()
 {
+    cout << "\nmain3\n";
     struct function_object {
         void operator()() const { cout << "I'm a function object\n"; }
     };
@@ -134,6 +217,7 @@ int main()
   try {
     main1();
     main2();
+    main3();
   } catch(std::exception& e) { 
     cerr << "main threw: " << e.what() << "\n";
  }
