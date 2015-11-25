7f41f0e3e524c5c85fedc751d6d23e90178e7724
CaptGreg <j.greg.blair@gmail.com>
2015-11-24 23:05:31 -0500

    add Korean digits, experient with Arabic (wchar_t digit array is 42 wchar_t long)

--- wfilestream.cpp
+++ wfilestream.cpp
@@ -3,10 +3,18 @@
 #include <fstream> 
 #include <iosfwd>   // wofstream
 #include <cstdlib>  // rand
-#include <string>
 using namespace std;
 
-const wchar_t* chineseDigits = L"零一二三四五六七八九"; // All 10 Chinese digits 0 through 9
+const wchar_t* chineseWCharDigits = L"零一二三四五六七八九"; // All 10 Chinese     digits 0 through 9
+const wchar_t* koreanWCharDigits  = L"공일이삼사오육칠팔구"; // All 10 Sino-Korean digits 0 through 9
+const wchar_t* arabicWCharDigits  = L"ثلاثاثنانواحدصفرستةخمسةأربعةتسعةثمانيةسبعة"; // All 10 Sino-Korean digits 0 through 9
+// Chinese: google translate
+// Korean http://www.omniglot.com/language/numbers/korean.htm
+// Arabic: google translate
+
+const std::wstring chineseWString = chineseWCharDigits;
+const std::wstring koreanWString  = koreanWCharDigits;
+const std::wstring arabicWString  = arabicWCharDigits;
 
 void WideCharSetup()
 {
@@ -20,50 +28,61 @@ void WideCharSetup()
   // std::wcerr.imbue( std::locale() ) ;      // not using wcerr
 
   // test it
-  wcout << "Chinese characters for 0 to 9: " << chineseDigits << endl;
+  #define P(L,D) wcout << #L " wchar_t characters for 0 to 9: " << D << " ==> " << wcslen(D) << " wchar_t\n";
+  P(Chinese, chineseWCharDigits)
+  P(Korean, koreanWCharDigits)
+  P(Arabic, arabicWCharDigits)
+
+  // wcout << "Chinese wchar_t characters for 0 to 9: " << chineseWCharDigits << endl;
+  // wcout << "Korean  wchar_t characters for 0 to 9: " << koreanWCharDigits << endl;
+  // wcout << "Arabic  wchar_t characters for 0 to 9: " << arabicWCharDigits << endl;
+  wcout << "\n";
+
+  wcout << "Chinese std::wstring characters for 0 to 9: " << chineseWString << endl;
+  wcout << "Korean  std::wstring characters for 0 to 9: " << koreanWString << endl;
+  wcout << "\n";
+
   for(int i = 0; i <= 9; i++) 
-    wcout << "Ascii digit " << i << " is Chinese character" << chineseDigits[i] << "\n";
+    wcout << "Ascii digit " << i 
+          << " is Chinese character"   << chineseWCharDigits[i] 
+          << ", Sino-Korean character" << koreanWCharDigits[i] 
+          // << ", Arabic character"      << arabicWCharDigits[i] 
+          << "\n";
 
   // works!
 }
 
-void WriteTestFile(string& fileChinese)
+template<typename T> // a function template
+void WriteTestFile(string& file, T& digits) // need T& so this works with 'const std::wstring'
 {
 
   // write a test file with 1 million Chinese characters
   size_t size = 1000000;
 
-  std::wofstream wos(fileChinese, wios::out | wios::trunc);
+  std::wofstream wos(file, wios::out | wios::trunc);
   if(wos.is_open()) {
-    for(int i = 0; i < size; i++) {
+    for(size_t i = 0; i < size; i++) {
        wchar_t wc;
 
-       wc =  chineseDigits[rand() % 10];
+       wc =  digits[rand() % 10];
        wos << wc;
     }
     wos.close();
   }
 }
 
-void Translate(string& fileChinese, string& fileOut)
+// void Translate(const wchar_t* digits, string& fileIn, string& fileOut)
+template<typename T> // a function template
+void Translate(T& digits, string& fileIn, string& fileOut)
 {
-///////////////////////////////////////////////////////////////////////////////
-  
-  // Write code here.
-  // Read in the file created above wide character by wide character 
-  // Write a second file out with the characters translated to ASCII digits.
-
-///////////////////////////////////////////////////////////////////////////////
-
-  // Solution:
-  std::wifstream wis(fileChinese, wios::in);
-  std::ofstream  os (fileOut,     ios::out | ios::trunc);
+  std::wifstream wis(fileIn,  wios::in);
+  std::ofstream  os (fileOut, ios::out | ios::trunc);
 
   if(wis.is_open() && os.is_open()) {
     wchar_t wc;
     while( wis >> wc ) {
-      for(int i = 0; i < 10; i++) {
-        if(wc == chineseDigits[i]) {
+      for(size_t i = 0; i < wcslen(digits); i++) {
+        if(wc == digits[i]) {
           char c = '0' + i;
           os << c;
           break;
@@ -71,29 +90,37 @@ void Translate(string& fileChinese, string& fileOut)
       }
     }
   } else {
-    throw string("problem creating '") + fileOut + "' or opening '" + fileChinese + "'\n";
+    throw string("problem creating '") + fileOut + "' or opening '" + fileIn + "'\n";
   }
 
   if(wis) wis.close();
   if(os)  os .close();
-///////////////////////////////////////////////////////////////////////////////
-
 }
 
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[], char* envp[])
+// int wmain(int argc,wchar_t*argv[],wchar_t*envp[]) // appears to be a Microsoft extension, not C++
 {
   try {
 
-    string rootFileName  =  string(argv[0]);
-    string fileChinese = rootFileName + ".dat.Chinese";
-    string fileOut     = fileChinese  + ".ASCII";
+    string rootFileName   =  string(argv[0]);
+
+    string fileChinese    = rootFileName + ".dat.Chinese";
+    string fileChineseOut = fileChinese  + ".ASCII";
+
+    string fileKorean     = rootFileName + ".dat.Korean";
+    string fileKoreanOut  = fileKorean   + ".ASCII";
 
     WideCharSetup();
 
-    WriteTestFile(fileChinese);
+    WriteTestFile(fileChinese, chineseWCharDigits);
+    WriteTestFile(fileChinese, chineseWString);
 
-    Translate(fileChinese, fileOut);
+    WriteTestFile(fileKorean,  koreanWCharDigits );
+    WriteTestFile(fileKorean,  koreanWString );
+
+    Translate(chineseWCharDigits, fileChinese, fileChineseOut);
+    Translate(koreanWCharDigits,  fileKorean,  fileKoreanOut);
 
   } catch(string& e) {
     cerr << e << "\n";
