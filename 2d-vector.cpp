f265a72df0b79d48bad34e24d651174494cc8858
Capt Greg <CaptGreg@github.com>
2015-12-08 11:39:05 -0500

    2d arrays and stl containers as a base class

--- 2d-vector.cpp
+++ 2d-vector.cpp
@@ -1,24 +1,102 @@
 #include <vector>
 #include <iostream>
+#include <memory>   // unique_ptr, ref
 using namespace std;
 
-// const int WIDTH  = 4000;
-// const int HEIGHT = 2000;
-const int WIDTH  = 2*1920;    // XHD (4K) Video resolution, true 4K is 4096x2160
+const int WIDTH  = 2*1920;    // XHD (4K) 3840x2160 Video resolution, true 4K is 4096x2160
 const int HEIGHT = 2*1080;
-
-vector    <vector<float> >                 vector2D;
 
 int main(int argc, char*argv[])
 {
-  // Set up sizes. (HEIGHT x WIDTH)
-  vector2D.resize(HEIGHT);
-  for (int y = 0; y < HEIGHT; ++y)
-    vector2D[y].resize(WIDTH);
+  {  // 2d array using std::vector
+     vector <vector<float> >  table;
 
-  for(int y = 0; y < HEIGHT; y++) {
-    for(int x = 0; x < WIDTH; x++) {
-      vector2D [y][x] = 1e6*y + x;
+    // Set up sizes. (HEIGHT x WIDTH)
+    table.resize(HEIGHT);
+    for (int y = 0; y < HEIGHT; ++y)
+      table[y].resize(WIDTH);
+
+    for(int y = 0; y < HEIGHT; y++) {
+      for(int x = 0; x < WIDTH; x++) {
+        table [y][x] = 1e6*y + x;
+      }
+    }
+    // auto access = [&] (int x, int y) { return ref(table[y][x]); };
+    // float& access = [&] (int x, int y) { return table[y][x]; };
+    #define ACCESS(X, Y) table[Y][X]
+    for(int y = 0; y < HEIGHT; y++) {
+      for(int x = 0; x < WIDTH; x++) {
+        ACCESS(x,y) = 1e6*y + x;
+      }
     }
   }
-}
+
+  { // map linear array to 2d array
+    float *table = new float[WIDTH * HEIGHT];
+
+    for(int y = 0; y < HEIGHT ; y++ )
+      for(int x = 0; x < WIDTH; x++)
+        table[y * WIDTH + x] = 1e6 * y + x;
+
+    delete [] table;
+  }
+
+  { // map linear smart point array to 2d array 
+  std::unique_ptr< float > table( new float[WIDTH*HEIGHT] );
+  for(int y = 0; y < HEIGHT ; y++ )
+    for(int x = 0; x < WIDTH; x++)
+      *(table.get() + y * WIDTH + x) = 1e6 * y + x;
+  } // smart pointer out of scope (automatically deleted)
+
+  { // map linear C++11 smart point array to 2d array 
+  std::unique_ptr< float[] > table( new float[WIDTH*HEIGHT] ); // [] setup for C++11 unique_ptr::operator[]
+  for(int y = 0; y < HEIGHT ; y++ )
+    for(int x = 0; x < WIDTH; x++)
+      table[y * WIDTH + x] = 1e6 * y + x;
+  } // smart pointer out of scope (automatically deleted)
+
+  { // 2d-array row table: (HEIGHT+1) alloc/dealloc calls
+    float **rowTable = new float*[ HEIGHT ];
+    for(int y =0; y < HEIGHT; y++)
+      rowTable[y] = new float[WIDTH];
+
+    for(int y = 0; y < HEIGHT ; y++ )
+      for(int x = 0; x < WIDTH; x++)
+        rowTable [y] [x] = 1e6 * y + x;
+
+    for(int y =0; y < HEIGHT; y++)
+      delete [] rowTable[y];
+    delete [] rowTable; 
+  }
+
+  { // rowtable+data allocated in one alloc/dealloc pair of calls
+    unsigned char *table = new unsigned char[  HEIGHT * sizeof(float*) +   WIDTH * HEIGHT * sizeof(float) ];
+
+    float** rowTable = (float**)table;
+    float*  data     = (float*) (table + HEIGHT * sizeof(float*));
+
+    for(int y =0; y < HEIGHT; y++)
+      rowTable[y] = data + y * WIDTH;
+
+    for(int y = 0; y < HEIGHT ; y++ )
+      for(int x = 0; x < WIDTH; x++)
+        rowTable [y] [x] = 1e6 * y + x;
+
+    delete [] table; 
+  }
+
+  { // rowtable+data allocated in one smart pointer
+    unique_ptr<unsigned char> table( new unsigned char[ HEIGHT * sizeof(float*) +  WIDTH * HEIGHT * sizeof(float) ] );
+
+    float** rowTable = (float**)  table.get();
+    float*  data     = (float*)  (table.get() + HEIGHT * sizeof(float*));
+
+    for(int y =0; y < HEIGHT; y++)
+      rowTable[y] = data + y * WIDTH;
+
+    for(int y = 0; y < HEIGHT ; y++ )
+      for(int x = 0; x < WIDTH; x++)
+        rowTable [y] [x] = 1e6 * y + x;
+  } // unique_ptr 'table' deleted when out of scope
+
+} // end main
---
+++ stl-base.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  class X {
+    string n;
+  public:
+  X() { cout << "ctor() " << n << "\n"; }
+  X(string&& name) : n(move(name))  { cout << "ctor(&&) " << n << "\n"; }
+  X(string& name)  : n(name)        { cout << "ctor(&)  " << n << "\n"; }
+
+  ~X() { cout << "dtor " << n << "\n"; }
+
+  X(const X& rhs)  { cout << "copy ctor " << n << " rhs=" << rhs.n << "\n"; n = rhs.n; }
+  X(X&& rhs)       { cout << "MOVE ctor " << n << " rhs=" << rhs.n << "\n"; n = move( rhs.n); rhs.n.clear();}
+
+  X&  operator= (const X& rhs)  { cout << "copy = op rhs=" << rhs.n << "\n"; if(this!=&rhs) n=rhs.n; return *this; }
+  X&& operator= (const X&& rhs) { cout << "MOVE = op rhs=" << rhs.n << "\n"; if(this!=&rhs) n=rhs.n; return move(*this); }
+  };
+
+  class Y {
+    vector<X> v;
+  public:
+    void push_back(const X&  x)                    { v.push_back(x);       }
+    void push_back(X&& x)                          { v.push_back(move(x)); }
+    vector<X>::iterator begin()                    { return v.begin();     }
+    vector<X>::iterator end()                      { return v.end();       }
+    const vector<X>::const_iterator cbegin() const { return v.cbegin();    }
+    const vector<X>::const_iterator cend()   const { return v.cbegin();    }
+  };
+
+  class Z : public vector<X> {
+  };
+
+  {
+    cout << "\n++++++++++++++++ Y block scope start\n";
+    X x1("x1"), x2("x2");
+    Y y; 
+    cout << "y.push_back(x1);\n";
+    y.push_back(x1); 
+    cout << "y.push_back(move(x2));\n";
+    y.push_back(move(x2));
+    cout << "++++++++++++++++ Y block scope over\n";
+    cout << "\n";
+  }
+  
+
+  {
+    cout << "\n++++++++++++++++ Z block scope start\n";
+    X x1("x1"), x2("x2");
+    Z z; 
+    cout << "z.push_back(x1);\n";
+    z.push_back(x1); 
+    cout << "z.push_back(move(x2));\n";
+    z.push_back(move(x2));
+    cout << "++++++++++++++++ Z block scope over\n";
+    cout << "\n";
+  }
+
+  cout << "main over\n";
+}
