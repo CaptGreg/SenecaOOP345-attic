e22863b7dd7ad8efc6b5988442a515ce9644b628
CaptGreg <john.blair@senecacollege.ca>
2016-02-29 13:27:32 -0500

    generic_interface.cpp - make it work with function objects.

--- generic_interface.cpp
+++ generic_interface.cpp
@@ -1,3 +1,6 @@
+// GB Feb 29, 2016
+//    1. added function code
+//    2. adopted boost code test  to C++11 environment
 // C++11 code from
 // https://gist.github.com/pfultz2/d0eea79bec9f7353339a
 // http://ideone.com/zTzwIL shows how to use it with boost.  GB adopted to C++11 template
@@ -10,85 +13,36 @@
 
 #include <string>
 #include <iostream>
-#include <utility> // std::declval
 
 // Update - how to do it in C++11 only
 // Paul Fultz II pointed out at twitter, that decltype and declval can do the same, 
 // simply by using auto and return type deduction:
-
-////////////////////////////////////////////////////////////////////////////////
-// http://en.cppreference.com/w/cpp/language/decltype 
-
-// decltype(arg) function only has a single argument.
-// comma operator ignores additional arguments
-
-// http://www.cplusplus.com/reference/utility/declval/
-// std::declval
-// template <class T>
-//   typename add_rvalue_reference<T>::type declval() noexcept;
-
-// Declaration value
-// Returns an rvalue reference to type T without referring to any object.
-// This function shall only be used in unevaluated operands (such as the 
-// operands of sizeof and decltype).
-// T may be an incomplete type.
-// This is a helper function used to refer to members of a class in unevaluated 
-// operands, especially when either the constructor signature is unknown or when 
-// no objects of that type can be constructed (such as for abstract base classes).
-
-// Parameters none
-// Return value An rvalue reference to type T.
-// The return type uses add_rvalue_reference to return T&& when T is an object type.
-////////////////////////////////////////////////////////////////////////////////
-// http://www.cplusplus.com/reference/utility/declval/
-// function template
-// <utility>
-// std::declval
-
-// template <class T>
-//   typename add_rvalue_reference<T>::type declval() noexcept;
-
-// Declaration value
-// Returns an rvalue reference to type T without referring to any object.
-
-// This function shall only be used in unevaluated operands (such as the operands of sizeof and decltype).
-
-// T may be an incomplete type.
-
-// This is a helper function used to refer to members of a class in unevaluated operands, especially when either the constructor signature is unknown or when no objects of that type can be constructed (such as for abstract base classes).
-
-// Parameters none
-
-// Return value An rvalue reference to type T.
-// The return type uses add_rvalue_reference to return T&& when T is an object type.
-////////////////////////////////////////////////////////////////////////////////
 
 template<class Interface>
 struct GenericInterface
 {
   template<class T = Interface>
   // auto query_interface() const -> decltype(std::declval<T>().interface(), std::string()) // comma operator, second arg ignored
-  auto query_interface() const -> decltype(std::declval<T>().interface())
-  {
-    return i.interface();
-  }
+  auto query_interface() const -> decltype( std::declval<T>().interface() )
+  { return i.interface(); }
+
   template<class T = Interface>
   // auto query_interface() const -> decltype(std::declval<T>().get_my_interface(), std::string()) // comma operator, second arg ignored
-  auto query_interface() const -> decltype(std::declval<T>().get_my_interface())
-  {
-    return i.get_my_interface();
-  }
+  auto query_interface() const -> decltype( std::declval<T>().get_my_interface() )
+  { return i.get_my_interface(); }
+
   template<class T = Interface>
   // auto query_interface() const -> decltype(std::declval<T>().myInterface(), std::string()) // comma operator, second arg ignored
-  auto query_interface() const -> decltype(std::declval<T>().myInterface())
-  {
-    return i.myInterface();
-  }
+  auto query_interface() const -> decltype( std::declval<T>().myInterface() )
+  { return i.myInterface(); } 
 
-  void print()
-  {
-    std::cout << query_interface() << "\n";
-  }
+  template<class T = Interface>  // GB function object, member function name is operator()
+  auto query_interface() const -> decltype( std::declval<T>().operator() () )
+  { return i(); } 
+
+  void print() { std::cout << query_interface() << "\n"; } 
+  
+  void iSet(const Interface& ii) { i = ii; } // GB - used to update 'i'
 
   Interface i;
 };
@@ -113,6 +67,53 @@ int main()
 
   std::cout << "calling gmyInterface.print()  --> ";
   gmyInterface.print();
+
+  // GB function object
+  std::cout << "\nFUNCTION OBJECTS\n";
+  struct FO {  // a rule-of-three function object
+    int i;
+
+    FO() : i(0) { std::cout << "\nFO(), i=" << i << "\n"; }
+
+    FO(int ii) : i(ii) { std::cout << "\nFO(" << ii << "), i=" << i << "\n"; }
+
+    ~FO() { std::cout << "\n~FO(), i=" << i << "\n"; }
+
+    FO& operator= (const FO& rhs) 
+    { 
+      std::cout << "\noperator= FO(const FO&), i=" << i << "," << rhs.i << "\n"; 
+      if(this!=&rhs) { i = rhs.i; }
+      return *this;
+    }
+
+    FO(const FO& rhs) { i=rhs.i; std::cout << "\nFO(const FO&), i=" << i << "\n"; }
+
+    void Set(int ii) { i = ii; }
+
+    std::string operator() ()      const { return std::to_string(i) + " function object operator()"; } 
+  };
+
+  GenericInterface <FO>             gfo;
+  std::cout << "calling gfo.print()  --> ";
+  gfo.print(); 
+
+  FO mfo(99);
+  GenericInterface <FO>             gmfo;
+  std::cout << "gmfo.print()  --> ";
+  gmfo.print(); 
+  std::cout << "update the GenericInterface with a non-default function object.\n";
+  gmfo.iSet(mfo);
+  std::cout << "gmfo.print()  --> ";
+  gmfo.print(); 
+  std::cout << "change the function object value: call mfo.Set(123)\n";
+  mfo.Set(123);
+  std::cout << "gmfo.print()  --> ";
+  gmfo.print(); 
+  std::cout << "the gmfo instance has a copy of the old value\n";
+  std::cout << "update the gmfo interface: call gmfo.iSet\n";
+  gmfo.iSet(mfo);
+  std::cout << "gmfo.print()  --> ";
+  gmfo.print(); 
 
   return 0;
 }
