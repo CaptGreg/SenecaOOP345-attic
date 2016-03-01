00541be908a5ff271f59e51c02ec41eb2b0b5b14
CaptGreg <john.blair@senecacollege.ca>
2016-03-01 18:41:38 -0500

    ctor_throw.cpp: experiment with private new/delete that traces new/delete execution.

--- ctor_throw.cpp
+++ ctor_throw.cpp
@@ -1,13 +1,38 @@
 // ctor throws implies the ctor failed, C++ does not need to call dtor, and it doesn't
 
+// The actual mechanics are:
+// 1. The ctor allocates space for any static sized private data members.
+// 2. the ctor code executes.
+// 3. If it throws, the provate data elelments are deallocated.
+
+#include <exception>   // for std::bad_alloc
+#include <cstdlib>     // for malloc() and free()
+#include <memory>      // unique_ptr/smart_ptr
+#include <new>         // prototypes for new/delete
 #include <iostream>
-#include <memory>
 
 using namespace std;
+
+void* operator new (size_t size)
+{
+  void *p=malloc(size); 
+  cout << "global new(" << size << ") = " <<p << "\n";
+  if (p==0) // did malloc succeed?
+    throw std::bad_alloc(); // ANSI/ISO compliant behavior
+  return p;
+}
+
+void operator delete (void *p)
+{
+  cout << "global delete(" << p << ")\n";
+  if(p) 
+    free(p); 
+}
 
 int main()
 {
   class X {
+  uint8_t buf1MB [1000000];
   public:
     X()  { cout << "X() - will now throw.\n"; throw nullptr; }
     ~X() { cout << "~X()\n"; };
@@ -21,7 +46,7 @@ int main()
 
   cout << "\nraw pointer to instance.\n";
   X* p = nullptr;
-  try { p = new X; } 
+  try { p = new X; cout<< "'new X' succeeded - calling 'delete'\n"; delete p; } 
   catch (...) { cout << "It threw.\n"; }
   cout << "p=" << (void*) p << "\n";
   cout << "Note: The dtor is never called\n";
