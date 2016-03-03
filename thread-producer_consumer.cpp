9e0b385d06b98bc53bc024dc2f91871c9b1d9954
Capt Greg <CaptGreg@github.com>
2016-03-03 17:49:48 -0500

    convert thread-producer-consumer.cpp to move semantics

--- thread-producer_consumer.cpp
+++ thread-producer_consumer.cpp
@@ -10,7 +10,8 @@
 
 // clang++ -std=c++11 producer_consumer.cpp -o producer_consumer -lrt
 
-//http://juanchopanzacpp.wordpress.com/2013/02/26/concurrent-queue-c11/
+// http://juanchopanzacpp.wordpress.com/2013/02/26/concurrent-queue-c11/
+// https://github.com/juanchopanza/cppblog/blob/master/Concurrency/Queue/producer_consumer1.cpp
 
 //
 // Copyright (c) 2013 Juan Palacios juan.palacios.puyana@gmail.com
@@ -18,24 +19,83 @@
 // - see < http://opensource.org/licenses/BSD-2-Clause>
 //
 
+// #pragma once
+// file threadsafe-queue.h
+// http://juanchopanzacpp.wordpress.com/2013/02/26/concurrent-queue-c11/
+//
+// Copyright (c) 2013 Juan Palacios juan.palacios.puyana@gmail.com
+// Subject to the BSD 2-Clause License
+// - see < http://opensource.org/licenses/BSD-2-Clause>
+//
+// GB - 2016 tidied up, converted to use move semantics
+
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+template <typename T>
+class Queue
+{
+  std::queue<T>           q;
+  std::mutex              m;
+  std::condition_variable c;
+ public:
+
+  T&& pop() 
+  {
+    std::unique_lock<std::mutex> mlock(m);
+    while (q.empty()) {
+      c.wait(mlock);
+    }
+    auto val = std::move(q.front());
+    q.pop();
+    return std::move(val);
+  }
+
+  void pop(T&& item)
+  {
+    std::unique_lock<std::mutex> mlock(m);
+    while (q.empty()) {
+      c.wait(mlock);
+    }
+    item = std::move(q.front());
+    q.pop();
+  }
+
+  void push(const T&& item)
+  {
+    std::unique_lock<std::mutex> mlock(m);
+    q.push(std::move(item));
+    mlock.unlock();
+    c.notify_one();
+  }
+  Queue()                          = default;
+  virtual ~Queue() {}
+  Queue(const Queue&)              = delete; // disable copy constructor
+  Queue(Queue&&)                   = delete; // disable move constructor
+  Queue&  operator= (const Queue&) = delete; // disable copy assignment
+  Queue&& operator= (Queue&&)      = delete; // disable move assignment
+}; // class Queue
+
 #include <thread>
 #include <mutex>   // for screenLock
 #include <iostream>
-#include "threadsafe-queue.h"
+// #include "threadsafe-queue.h"
 
 std::mutex screenLock;
 
-void produce(Queue<int>& q, unsigned totalWork) {
-  for (int i = 0; i< totalWork; ++i) {
+void produce(Queue<unsigned>& q, unsigned totalWork) {
+  for (unsigned i = 0; i< totalWork; i++) {
     {std::unique_lock<std::mutex> myTurn(screenLock);
       std::cout << "Pushing " << i << "\n";
     }
-    q.push(i);
+    q.push(std::move(i));
   }
 }
 
-void consume(Queue<int>& q, unsigned id, unsigned workUnits) {
-  for (int i = 0; i< workUnits; ++i) {
+void consume(Queue<unsigned>& q, unsigned id, unsigned workUnits) {
+  for (unsigned i = 0; i< workUnits; i++) {
     auto item = q.pop();
     {std::unique_lock<std::mutex> myTurn(screenLock);
       std::cout << "Consumer " << id << " popped " << item << "\n";
@@ -45,37 +105,21 @@ void consume(Queue<int>& q, unsigned id, unsigned workUnits) {
 
 int main()
 {
-  Queue<int> q;
+  Queue<unsigned> q;
 
-  using namespace std::placeholders;
-
-  unsigned totalWork  = 15; // 10000;
-  unsigned numThreads = 5;
-  unsigned workUnits  = totalWork / numThreads;
+  unsigned TOTALWORK  = 15; // 10000;
+  unsigned NUMTHREADS = 5;
+  unsigned WORKUNITS  = TOTALWORK / NUMTHREADS;
   
   // consumer threads
-  // std::thread consumer1( std::bind(&consume, std::ref(q), 1, workUnits) );
-  // std::thread consumer2( std::bind(&consume, std::ref(q), 2, workUnits) );
-  // std::thread consumer3( std::bind(&consume, std::ref(q), 3, workUnits) );
-  // std::thread consumer4( std::bind(&consume, std::ref(q), 4, workUnits) );
-  // std::thread consumer5( std::bind(&consume, std::ref(q), 5, workUnits) );
-
-  std::unique_ptr<std::thread[]> consumers(new std::thread[numThreads]);
-  for(int t = 0; t < numThreads; t++)
-    consumers[t] = std::thread( std::bind(&consume, std::ref(q), t+1, workUnits) );
+  std::unique_ptr<std::thread[]> consumers(new std::thread[NUMTHREADS]);
+  for(unsigned t = 0; t < NUMTHREADS; t++)
+    consumers[t] = std::thread( std::bind(&consume, std::ref(q), t+1, WORKUNITS) );
 
   // producer thread
-  std::thread prod1( std::bind(produce, std::ref(q), totalWork) );
+  std::thread producer( std::bind(produce, std::ref(q), TOTALWORK) );
+  producer.join();
 
-  prod1.join();
-
-  for(int t = 0; t < numThreads; t++)
+  for(unsigned t = 0; t < NUMTHREADS; t++)
     consumers[t].join();
-
-  // consumer1.join();
-  // consumer2.join();
-  // consumer3.join();
-  // consumer4.join();
-  // consumer5.join();
-
 }
--- threadsafe-queue.h
+++ threadsafe-queue.h
@@ -16,44 +16,41 @@
 template <typename T>
 class Queue
 {
+  std::queue<T>           q;
+  std::mutex              m;
+  std::condition_variable c;
+ 
  public:
 
   T pop() 
   {
-    std::unique_lock<std::mutex> mlock(mutex_);
-    while (queue_.empty())
-    {
-      cond_.wait(mlock);
+    std::unique_lock<std::mutex> mlock(m);
+    while (q.empty()) {
+      c.wait(mlock);
     }
-    auto val = queue_.front();
-    queue_.pop();
+    auto val = q.front();
+    q.pop();
     return val;
   }
 
   void pop(T& item)
   {
-    std::unique_lock<std::mutex> mlock(mutex_);
-    while (queue_.empty())
-    {
-      cond_.wait(mlock);
+    std::unique_lock<std::mutex> mlock(m);
+    while (q.empty()) {
+      c.wait(mlock);
     }
-    item = queue_.front();
-    queue_.pop();
+    item = q.front();
+    q.pop();
   }
 
   void push(const T& item)
   {
-    std::unique_lock<std::mutex> mlock(mutex_);
-    queue_.push(item);
+    std::unique_lock<std::mutex> mlock(m);
+    q.push(item);
     mlock.unlock();
-    cond_.notify_one();
+    c.notify_one();
   }
   Queue()=default;
   Queue(const Queue&) = delete;            // disable copying
   Queue& operator=(const Queue&) = delete; // disable assignment
-  
- private:
-  std::queue<T>           queue_;
-  std::mutex              mutex_;
-  std::condition_variable cond_;
 };
