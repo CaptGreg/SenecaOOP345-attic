2319f1cfd9d36f9610afd1f427764ef6a34d5d18
CaptGreg <john.blair@senecacollege.ca>
2016-03-13 11:37:12 -0400

    convert sort code to function templates, tidy unique_ptr.cpp

--- sorts.cpp
+++ sorts.cpp
@@ -1,51 +1,64 @@
 // http://stackoverflow.com/questions/244252/a-good-reference-card-cheat-sheet-with-the-basic-sort-algorithms-in-c
 
-#include <stdlib.h>
-#include <string.h>
-#include <memory.h> // malloc
+// GB ported C-code to C++ function templates, new/delete
 
-static void swap(int *a, int *b) {
+
+#include <cstring>     // memcpy
+#include <iostream>
+
+using namespace std;
+
+template <typename T>   // function template
+static void swap(T* a, T* b) {
     if (a != b) {
-        int c = *a;
+        T c = *a;
         *a = *b;
         *b = c;
     }
 }
 
-void bubblesort(int *a, int l) {
-    int i, j;
+template <typename T>   // function template
+void bubblesort(T* a, int l) {
+    int i, j;  // GB cannot be unsigned (or size_t)
 
-    for (i = l - 2; i >= 0; i--)
+    for (i = l - 2; i >= 0; i--)  // GB NOTE >=0 terminate condition
         for (j = i; j < l - 1 && a[j] > a[j + 1]; j++)
             swap(a + j, a + j + 1);
 }
 
-void selectionsort(int *a, int l) {
-    int i, j, k;
+template <typename T>   // function template
+void selectionsort(T* a, size_t l) {
+    size_t i, j, k;
     for (i = 0; i < l; i++) {
-        for (j = (k = i) + 1; j < l; j++)
+        for (j = (k = i) + 1; j < l; j++) {
             if (a[j] < a[k])
                 k = j;
+        }
         swap(a + i, a + k);
     }
 }
 
-static void hsort_helper(int *a, int i, int l) {
+template <typename T>   // function template
+static void hsort_helper(T* a, int i, int l) {
     int j;
 
-    for (j = 2 * i + 1; j < l; i = j, j = 2 * j + 1)
-        if (a[i] < a[j])
-            if (j + 1 < l && a[j] < a[j + 1])
+    for (j = 2 * i + 1; j < l; i = j, j = 2 * j + 1) {
+        if (a[i] < a[j]) {
+            if (j + 1 < l && a[j] < a[j + 1]) {
                 swap(a + i, a + ++j);
-            else
+            } else {
                 swap(a + i, a + j);
-        else if (j + 1 < l && a[i] < a[j + 1])
+            }
+        } else if (j + 1 < l && a[i] < a[j + 1]) {
             swap(a + i, a + ++j);
-        else
+        } else {
             break;
+        }
+    }
 }
 
-void heapsort(int *a, int l) {
+template <typename T>   // function template
+void heapsort(T* a, int l) {
     int i;
 
     for (i = (l - 2) / 2; i >= 0; i--)
@@ -57,8 +70,9 @@ void heapsort(int *a, int l) {
     }
 }
 
-static void msort_helper(int *a, int *b, int l) {
-    int i, j, k, m;
+template <typename T>   // function template
+static void msort_helper(T* a, T* b, size_t l) {
+    size_t i, j, k, m;
 
     switch (l) {
         case 1:
@@ -74,20 +88,24 @@ static void msort_helper(int *a, int *b, int l) {
         a[i] = b[j < m && !(k < l && b[j] > b[k]) ? j++ : k++];
 }
 
-void mergesort(int *a, int l) {
-    int *b;
+template <typename T>   // function template
+void mergesort(T* a, size_t l) {
+    T *b;
 
     if (l < 0)
         return;
 
-    b = (int*) malloc(l * sizeof(int));
-    memcpy(b, a, l * sizeof(int));
+    // b = (T*) malloc(l * sizeof(T));
+    b = new T[l];
+    memcpy(b, a, l * sizeof(T));
     msort_helper(a, b, l);
-    free((void*) b);
+    // free((void*) b);
+    delete [] b;
 }
 
-static int pivot(int *a, int l) {
-    int i, j;
+template <typename T>   // function template
+static int pivot(T* a, size_t l) {
+    size_t i, j;
 
     for (i = j = 1; i < l; i++)
         if (a[i] <= a[0])
@@ -98,30 +116,32 @@ static int pivot(int *a, int l) {
     return j;
 }
 
-void quicksort(int *a, int l) {
-    int m;
-
+template <typename T>   // function template
+void quicksort(T* a, size_t l) {
     if (l <= 1)
         return;
 
-    m = pivot(a, l);
+    size_t m = pivot(a, l);
     quicksort(a, m - 1);
     quicksort(a + m, l - m);
 }
 
-struct node {
-    int value;
-    struct node *left, *right;
-};
-
-void btreesort(int *a, int l) {
-    int i;
+template <typename T>   // function template
+void btreesort(T* a, size_t l) {
+    size_t i;
+    struct node {
+        T value;
+        struct node *left, *right;
+    };
     struct node *root = NULL, **ptr;
 
     for (i = 0; i < l; i++) {
         for (ptr = &root; *ptr;)
             ptr = a[i] < (*ptr)->value ? &(*ptr)->left : &(*ptr)->right;
+
         *ptr = (struct node*) malloc(sizeof(struct node));
+        // *ptr = new node;
+
         **ptr = (struct node){.value = a[i]};
     }
 
@@ -130,21 +150,76 @@ void btreesort(int *a, int l) {
         for (ptr = &root; (*ptr)->left; ptr = &(*ptr)->left);
         a[i] = (*ptr)->value;
         node = (*ptr)->right;
+
         free((void*) *ptr);
-        (*ptr) = node;
+        // delete ptr;
+
+        (*ptr) = node;  // GB: Delete node then store something at that address?
+        // aborts with new/delete.  Runs with malloc/free.
+        // munmap_chunk(): invalid pointer: 0x0000000000f49c98 ***
+        // /bin/bash: line 1:  7236 Aborted
     }
 }
 
+template <typename T>   // function template
+void shellsort(T a[], const int size)// Nothing to do with shells.  Named after D.H. Shell
+{
+  int i, j, inc;
+  T tmp;
+ 
+  inc = 3;
+  while (inc > 0) {
+    for (i=0; i < size; i++) {
+      j = i;
+      tmp = a[i];
+      while ((j >= inc) && (a[j-inc] > tmp)) {
+        a[j] = a[j - inc];
+        j = j - inc;
+      }
+      a[j] = tmp;
+    }
+    if (inc/2 != 0)
+      inc = inc/2;
+    else if (inc == 1)
+      inc = 0;
+    else
+      inc = 1;
+  }
+}
 
 int main(int argc, char**argv)
 {
-  int *a;
-  int l;
+  int a[] = {42,19,2,66,1,33,8,5,19};
+  int l = sizeof(a) / sizeof(a[0]);
+  int b[l];
+  memcpy(b,a,sizeof(a));
+
+  cout << "input data "; for(auto e: a) cout << e << " "; cout << "\n\n";
 
   bubblesort(a, l);
+  cout << "bubblesort "; for(auto e: a) cout << e << " "; cout << "\n";
+
+  memcpy(a,b,sizeof(a));
   selectionsort(a, l);
+  cout << "selectionsort "; for(auto e: a) cout << e << " "; cout << "\n";
+
+  memcpy(a,b,sizeof(a));
   heapsort(a, l);
+  cout << "heapsort "; for(auto e: a) cout << e << " "; cout << "\n";
+
+  memcpy(a,b,sizeof(a));
   mergesort(a, l);
+  cout << "mergesort "; for(auto e: a) cout << e << " "; cout << "\n";
+
+  memcpy(a,b,sizeof(a));
   quicksort(a, l);
+  cout << "quicksort "; for(auto e: a) cout << e << " "; cout << "\n";
+
+  memcpy(a,b,sizeof(a));
   btreesort(a, l);
+  cout << "btreesort "; for(auto e: a) cout << e << " "; cout << "\n";
+
+  memcpy(a,b,sizeof(a));
+  shellsort(a, l);
+  cout << "shellsort "; for(auto e: a) cout << e << " "; cout << "\n";
 }
--- unique_ptr.cpp
+++ unique_ptr.cpp
@@ -1,51 +1,54 @@
 // copy ctor, assignment operators with a unique_ptr data member
 
-#include <memory>      // std::unique_ptr
+#include <memory>      // unique_ptr
 #include <cstring>     // memcpy
 #include <iostream>
 #include <exception>
 
+using namespace std;
+
 class X {
-    std::unique_ptr<uint8_t> data; 
-    static const size_t SIZE = 1LL * 1024LL * 1024LL * 1024LL;  // 1 GB
+    unique_ptr<uint8_t> data; 
+    static const size_t SIZE = 1LL * 1000LL * 1000LL * 1000LL;  // 1e9 (GB)
 public:
     X()                          // constructor
     { 
-      std::cout << "   X ctor allocating " << SIZE << " bytes\n";
-      data = std::unique_ptr<uint8_t> (new uint8_t[SIZE]);
+      cout << "   X ctor allocating " << SIZE << " bytes\n";
+      data = unique_ptr<uint8_t> (new uint8_t[SIZE]);
     }
     ~X()                         // destructor
     { 
-        std::cout << "   X dtor";
+        cout << "   X dtor";
         if(data) {
-           std::cout <<  " releasing " << SIZE << " bytes\n";
-           data.reset(nullptr);
+           cout <<  " releasing " << SIZE << " bytes\n";
+           data.reset(nullptr);  
         } else {
-           std::cout <<  " ZOMBIE\n";
+           cout <<  " ZOMBIE\n";
         }
     }
     X(X &x) //copy constructor                         // deep copy
     { 
-        std::cout << "   X copy ctor allocate and deep copy of " << SIZE << " bytes\n";
-        data = std::unique_ptr<uint8_t> (new uint8_t[SIZE]);
+        cout << "   X copy ctor allocate and deep copy of " << SIZE << " bytes\n";
+        data = unique_ptr<uint8_t> (new uint8_t[SIZE]);
         memcpy( (void*) data.get(),  (void*) x.data.get(), SIZE * sizeof(*data));
     }
     X& operator=(const X &x) // copy assignment operator // deep copy
     { 
-        std::cout << "   X assignment operator deep copy of " << SIZE << " bytes\n";
+        cout << "   X assignment operator deep copy of " << SIZE << " bytes\n";
         // no need to de-allocate and immediately re-allocate the same size buffer
         memcpy( (void*) data.get(),  (void*) x.data.get(), SIZE * sizeof(*data));
         return *this;
     }
-    X(X &&x) : data(std::move(x.data)) // move constructor      
+    X(X &&x) : data(move(x.data)) // move constructor      
     { 
-        std::cout << "   X move ctor\n";
+        cout << "   X move ctor\n";
     }
-    X& operator=(X &&x)                // move assignment operator
+    X&& operator=(X &&x)                // move assignment operator
     { 
-        std::cout << "   X move assignment operator\n";
-        data = std::move(x.data);
-        return *this;
+        cout << "   X move assignment operator\n";
+        data = move(x.data);
+        x.data.reset(nullptr);  
+        return move( *this );
     }
     uint8_t get(size_t i)            { return *(data.get()+i);     }
     void    set(size_t i, uint8_t v) {        *(data.get()+i) = v; }
@@ -54,34 +57,35 @@ public:
 
 int main()
 {
-  std::cout << "start-of-main\n";
+  cout << "start-of-main\n";
+  atexit([] () { cout << "atexit callback\n"; } );
 
   try {
-    std::cout << "X x;\n";
+    cout << "X x;\n";
     X x; 
     x.set(1000,42);
 
-    std::cout << "X xx(x);\n";
+    cout << "X xx(x);\n";
     X xx(x);  
-    std::cout << "xx(1000) = " << xx.get(1000) << "\n";
+    cout << "xx(1000) = " << xx.get(1000) << " (ascii 42 is character '*')\n";
 
-    std::cout << "x = xx;\n";
+    cout << "x = xx;\n";
     x = xx;
-    std::cout << "x(1000) = " << x.get(1000) << "\n";
+    cout << "x(1000) = " << x.get(1000) << " (ascii 42 is character '*')\n";
   
-    std::cout << "X xxx(std::move(x));\n";
-    X xxx(std::move(x));
-    std::cout << "xxx(1000) = " << xxx.get(1000) << "\n";
+    cout << "X xxx(move(x));\n";
+    X xxx(move(x));
+    cout << "xxx(1000) = " << xxx.get(1000) << " (ascii 42 is character '*')\n";
 
-    std::cout << "xx = std::move(xxx);\n";
-    xx = std::move(xxx);
-    std::cout << "xx(1000) = " << xx.get(1000) << "\n";
+    cout << "xx = move(xxx);\n";
+    xx = move(xxx);
+    cout << "xx(1000) = " << xx.get(1000) << " (ascii 42 is character '*')\n";
 
-    std::cout << "end-of-try-catch block\n";
+    cout << "end-of-try-catch block\n";
 
-  } catch (std::exception& e) {
-    std::cerr << e.what() << "\n";
+  } catch (exception& e) {
+    cerr << e.what() << "\n";
   }
 
-  std::cout << "end-of-main\n";
+  cout << "end-of-main\n";
 }
