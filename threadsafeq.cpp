716d2cbf13d611fe42ea8a9b10cbbc7cec2dd7db
CaptGreg <john.blair@senecacollege.ca>
2016-03-23 20:42:58 -0400

    threadsafeq.cpp -  simple thread safe q w/wo try_lock/condition_variable wait

--- threadsafeq.cpp
+++ threadsafeq.cpp
@@ -2,6 +2,7 @@
 
 #include <queue>
 #include <mutex>
+#include <condition_variable> // GB  - needed for tsqueue Pop that waits if q empty
 
 // includes for test program
 #include <vector>
@@ -9,15 +10,25 @@
 #include <iostream>
 #include <sstream>
 
+// What is the difference between lock_guard and unique_lock?
+// The difference is that you can lock and unlock a std::unique_lock. 
+// Must use a std::unique_lock for condition variables.
+// std::lock_guard will be locked only once on construction and unlocked on destruction.
+
+// Always use lock_guard, unless you need the capabilities of unique_lock. 
+// A condition_variable needs a unique_lock.
+
 template <typename T>
 class tsQueue {  // thread safe queue
-  std::queue<T> q;
+protected:
+  std::queue<T>      q;
   mutable std::mutex m;
 public:
-  ~tsQueue() {
+  virtual ~tsQueue() {
     std::lock_guard<std::mutex> smartLock(m);
-    while( ! q.empty() )
+    while( not q.empty() ) {
       q.pop();
+    }
   }
   bool empty() const {
     std::lock_guard<std::mutex> smartLock(m);
@@ -36,23 +47,54 @@ public:
   }
 };
 
+template <typename T>  // GB
+class tsQueueWait : public tsQueue<T> {  // tsqueue Pop that waits for Push if q empty
+  std::condition_variable cv;
+  bool shutdown;
+public:
+  tsQueueWait() : shutdown(false) {}
+
+  virtual ~tsQueueWait() {
+    shutdown = true;
+    cv.notify_all();
+  }
+
+  void Push( T& t ) {
+    std::lock_guard<std::mutex> smartLock(tsQueue<T>::m);
+    tsQueue<T>::q.push( std::move( t ) );
+    cv.notify_one();
+  }
+
+  bool Pop( T& t ) {
+    std::unique_lock<std::mutex> smartLock(tsQueue<T>::m);
+    while(tsQueue<T>::q.empty() && not shutdown)
+      cv.wait(smartLock);
+
+    if(shutdown) return false;
+
+    t = std::move( tsQueue<T>::q.front() );
+    tsQueue<T>::q.pop();
+    return true;
+  }
+};
+
 int main()
 {
 // runtime error: 
 // terminate called without an active exception
-// add try catch
 
-// No, try-catch did not help but gdb did:
-// terminiate called by thread destructor
+// Declaring 'std::vector<std::thread> tidVector(N);' was the problem.
+// Works fine declared as 'std::vector<std::thread> tidVector;'
 
-// Declaring 'std::vector<std::thread> t(N);' was the problem.
-// Works fine declared as 'std::vector<std::thread> t;'
+// Reason:  queues N thread reads and N thread writes
+// vector<thread> tidVector(N) should be 2*N, not N
 
+  const int N = 1000;
   try {
-    tsQueue<int> q;
+    // tsQueue<int> q;
+    tsQueueWait<int> q;  // default code works with tsQueueWait
   
-    const int N = 1000;
-    std::vector<std::thread> t;
+    std::vector<std::thread> tidVector;
    
     auto pushQ= [&q] (int i) {q.push(i);};
     auto popQ = [&q] (int i)
@@ -71,16 +113,47 @@ int main()
  
     // test out the tryPop mechanism.
     // queue the pops before the pushes.
+    // Q N reads
     for(int i = 0; i < N ; i++) {
-      t.push_back( std::thread( popQ, i ) );
+      tidVector.push_back( std::thread( popQ, i ) );
     }
+    // issue N writes
     for(int i = 0; i < N ; i++) {
-      t.push_back( std::thread( pushQ, i ) );
+      tidVector.push_back( std::thread( pushQ, i ) );
     }
-    for(auto& e : t) e.join();
-
+    for(auto& e : tidVector) e.join();
 
     std::cout << "\n";
+  } catch(std::exception& e) {
+    std::cerr << "program threw: " << e.what() << "\n";
+  } catch(...) {
+    std::cerr << "program threw unspecified error" << "\n";
+  }
+
+  try {
+    tsQueueWait<int> q;
+  
+    std::vector<std::thread> tidVector;
+    auto PushQ= [&q] (int t) {q.Push(t);};
+    auto PopQ = [&q] (int t)
+      {
+        int i;
+        q.Pop(i);
+        std::ostringstream ss;
+        ss << "thread " << t << " popped " << i << " (after possible wait) " << "\n";
+        std::cout << ss.str();
+      }; 
+
+    // Q N reads
+    for(int i = 0; i < N ; i++) {
+      tidVector.push_back( std::thread( PopQ, i ) );
+    }
+    // issue N writes
+    for(int i = 0; i < N ; i++) {
+      tidVector.push_back( std::thread( PushQ, i ) );
+    }
+    for(auto& e : tidVector) e.join();
+
 
   } catch(std::exception& e) {
     std::cerr << "program threw: " << e.what() << "\n";
