ca2c415fb1deb14c9dc2a04ebb43434633dc0c30
CaptGreg <john.blair@senecacollege.ca>
2017-06-20 18:52:15 -0400

    play with setting multiple promise values - not successful

--- promise-set_value_at_thread_exit.cpp
+++ promise-set_value_at_thread_exit.cpp
@@ -6,17 +6,25 @@
  
 int main()
 {
-    // using namespace std::chrono_literals;               // unknown namespace
-    using namespace std;                                   // GB
-    std::promise<int> p;
-    std::future<int> f = p.get_future();
-    std::thread([&p] {
-          // std::this_thread::sleep_for(1s);              // GB 1s not defined
-          std::this_thread::sleep_for(chrono::seconds(1)); // GB this works
-          p.set_value_at_thread_exit(9);
-    }).detach();
+    // using namespace std::chrono_literals;                    // unknown namespace
+    std::promise<int> prom;
+    std::future<int> fut = prom.get_future();
+    std::thread(
+      [&prom] {
+          std::cout << "  thread alive... " << std::flush;
+          // prom.set_value(99);
+          // std::this_thread::sleep_for(1s);                   // GB 1s not defined, -std=c++17 doesn't help
+          std::this_thread::sleep_for(std::chrono::seconds(2)); // GB this works
+          prom.set_value_at_thread_exit(9);
+          std::cout << "thread exit value set..." << std::flush;
+          std::this_thread::sleep_for(std::chrono::seconds(2));
+          std::cout << "   thread exiting\n" << std::flush;
+      }
+    ).detach();
  
+    // can only retrieve fut.get() once
+    // retrieves first value set by prom.set_value(...)
     std::cout << "Waiting..." << std::flush;
-    f.wait();
-    std::cout << "Done!\nResult is: " << f.get() << '\n';
+    fut.wait();
+    std::cout << "Done!\nResult is: " << fut.get() << '\n';
 }
