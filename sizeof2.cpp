8a9bc7a510e1532038c94772f4baf978dadb16d6
CaptGreg <john.blair@senecacollege.ca>
2017-08-04 08:48:52 -0400

    downsize the buffer size in sizeof2.cpp

--- sizeof2.cpp
+++ sizeof2.cpp
@@ -1,18 +1,18 @@
-//  this 64-bit 5GB program runs on 4GB mem computer using virtual memory (swap space)
+//  this 64-bit 3GB program runs on 4GB mem computer using virtual memory (swap space)
 
 #ifdef __cplusplus
         #include <iostream>
         using namespace std;
 
-        #define P(T) cout << "sizeof(" #T ")=" << sizeof(T) << "\n";
+        #define P(QUACK) cout << "CPP: sizeof(" #QUACK ")=" << sizeof(QUACK) << "\n";
 #else
         #include <stdio.h>
-        #define P(T) printf("sizeof(" #T ")=%lu\n", (long) sizeof(T) );
+        #define P(ARF) printf("C: sizeof(" #ARF ")=%lu\n", (long) sizeof(ARF) );
 #endif
 
 
 // char big_buf[5L * 1000L * 1000L * 1000L];   //  5 Gigbytes, see what the compiler generates in 32-bit mode
-char big_buf[5LL * 1000LL * 1000LL * 1000LL];  //  5 Gigbytes guaranteed in 32-bit mode
+char big_buf[3LL * 1000LL * 1000LL * 1000LL];  //  3 Gigbytes guaranteed in 32-bit mode
 
 
 int main(int argc, char ** argv)
