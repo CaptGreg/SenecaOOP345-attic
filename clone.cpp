ab9e54a43ac69e34d79c1a6b449f7fb94a0b1fe2
CaptGreg <john.blair@senecacollege.ca>
2017-08-07 11:07:00 -0400

    add more clone functions using smart pointers

--- clone.cpp
+++ clone.cpp
@@ -1,57 +1,104 @@
 // clone member function. 
-// see inheritance section, page 59+60, of text book 
-// and http://www.cplusplus.com/forum/articles/18757/
+// http://www.cplusplus.com/forum/articles/18757/
+
+// Covariant Return Type: ABC declares B*clone() + class X declares X*clone()
 
 #include <iostream>
-#include <vector> // works with vector, a contiguous memory table
 #include <list>   // works with linked lists, not a contiguous memory table
 #include <memory>
 
 using namespace std;
 
-class B {  // an ABC, (abstract base class)
+class B {  // an abstract base class
 public:
-  virtual ~B() {cout<<"B:dtor\n";}   // ABC: virtual destructors to insure the dtor for any B* pointer to a derived class is called 
+  // ABC: virtual destructors to insure the dtor for any B* pointer to a derived class is called 
+  virtual ~B() {cout<<"B:dtor\n";}   
   virtual B* clone() = 0;
 };
 
 class X : public B {
-  list<uint16_t> v;  // or vector<uint16_t> v;
+  list<uint16_t> v;
 public:
-  ~X() {cout<<"X:dtor\n";}
+  ~X   ()                        { cout<<"X:dtor\n"; }
 
-//B* clone() { return new X(*this); }  // see page 59+60 of text book.
-  X* clone() { return new X(*this); }  // Covariant Return Type: ABC declares B*clone() + class X declares X*clone()
+  X*   clone            ()       { return new X(*this); }  
 
-  void operator+=(int vv) { v.push_back(vv); }
-  void print() { for(auto element : v) cout << element;  cout << "\n"; }
+  unique_ptr<X> cloneUP ()       { return unique_ptr<X> ( new X(*this) ); }
+
+  void operator +=      (int vv) { v.push_back(vv); }
+  void print            ()       { for(auto& e : v) cout << e;  cout << "\n"; }
 };
 
 int main(int argc, char**argv)
 {
+  atexit( [] {cout << "atexit callback called: main over, all dtors should have been called\n";});
+
   X x;
   x += 1;
   x += 2;
   x += 3;
   cout << "x=";  x.print();
-  
-  // X* xClone =  x.clone(); 
-  // error: cannot initialize a variable of type 'X *' with an rvalue of type 'B *'
-  // fix error with a cast
-  // X* xClone = dynamic_cast<X*> ( x.clone() );  // clone calls new, must delete.
-  unique_ptr<X> xClone(dynamic_cast<X*> ( x.clone() ) );
 
-  cout << "xClone="; xClone->print();
+  {
+    cout << "+++++++++++++++++++++++++++++++++++++++++\n";
+    cout << "+ clone result stored in a raw pointer. +\n";
+    cout << "+++++++++++++++++++++++++++++++++++++++++\n";
+    X* xClone =  x.clone(); 
 
-  cout << "adding 4 to xClone\n";
-  *xClone += 4;
-  cout << "xClone="; xClone->print();
+    cout << "xClone="; xClone->print();
 
-  cout << "if x prints 123 + xClone prints 1234, they are different\n";
-  cout << "This means STL did a deep copy during the 'clone of x' operation.\n";
-  cout << "x=";  x.print();
+    cout << "adding 4 to xClone\n";
+    *xClone += 4;
+    cout << "xClone="; xClone->print();
 
-  // yes, x printed 123.
+    cout << "if xClone prints 1234, they are different\n";
+    cout << "This means STL did a deep copy during the 'clone of x' operation.\n";
 
-  // delete xClone;  // clone called new, must delete
+    // yes, x prints 123, xClone prints 1234
+
+    delete xClone;  // clone called new
+  }
+
+
+  {
+    cout << "+++++++++++++++++++++++++++++++++++++++++++\n";
+    cout << "+ clone result stored in a smart pointer. +\n";
+    cout << "+++++++++++++++++++++++++++++++++++++++++++\n";
+
+    unique_ptr<X> xClone( x.clone() );
+
+    cout << "xClone="; xClone->print();
+
+    cout << "adding 4 to xClone\n";
+    *xClone += 4;
+    cout << "xClone="; xClone->print();
+
+    cout << "if xClone prints 1234, they are different\n";
+    cout << "This means STL did a deep copy during the 'clone of x' operation.\n";
+
+    // yes, x prints 123, xClone prints 1234
+  }
+
+
+  {
+    cout << "+++++++++++++++++++++++++++++++++\n";
+    cout << "+ smart pointer clone function. +\n";
+    cout << "+++++++++++++++++++++++++++++++++\n";
+
+    unique_ptr<X> xClone( x.cloneUP() );
+
+    cout << "xClone="; xClone->print();
+
+    cout << "adding 4 to xClone\n";
+    *xClone += 4;
+    cout << "xClone="; xClone->print();
+
+    cout << "if xClone prints 1234, they are different\n";
+    cout << "This means STL did a deep copy during the 'clone of x' operation.\n";
+
+    // yes, x prints 123, xClone prints 1234
+  }
+
+  cout << "Main exiting, dtor for instance 'x' should be called once main returns\n";
+  return 0;
 }
