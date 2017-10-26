0b738610028a0a862a33d20c8d53d57999b56d3d
CaptGreg <john.blair@senecacollege.ca>
2017-10-25 20:18:23 -0400

    add split string on string delimiter

--- splitstring.cpp
+++ splitstring.cpp
@@ -1,30 +1,78 @@
-// http://www.cplusplus.com/articles/2wA0RXSz/
+// GB select ignore empty variables if desired
 
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
 
-const vector<string> explode(const string& s, const char& c)
+const vector<string> splitChar(const string& s, const char& delimiter, bool bIgnoreEmpty = false)
 {
-	string buff{""};
+  string token;
 	vector<string> v;
 	
-	for(auto n:s)
-	{
-		if(n != c) buff+=n; else
-		if(n == c && buff != "") { v.push_back(buff); buff = ""; }
+	for(auto n:s) {
+		if(n != delimiter) {
+      token += n; 
+    } else if(not token.empty()) {  // not empty, add token
+      v.push_back( move(token) );
+    } else if(not bIgnoreEmpty) {   // empty, add empty token anyway
+      v.push_back( move(token) );
+    }
 	}
-	if(buff != "") v.push_back(buff);
+	if(not token.empty()) 
+    v.push_back( move(token) );
 	
 	return v;
 }
 
+vector<string> splitString(string& s, string delimiter, bool bIgnoreEmpty = false) // destroys s
+{
+  string token;
+  vector<string> v;
+
+  size_t pos = 0;
+  while ((pos = s.find(delimiter)) != string::npos) {
+      token = s.substr(0, pos);
+      if(not token.empty()) {          // not empty, add token
+        v.push_back( move(token) );
+      } else if(not bIgnoreEmpty) {    // empty, add empty token anyway
+        v.push_back( move(token) );
+      }
+      s.erase(0, pos + delimiter.length());
+  }
+  v.push_back( move(s) );
+  return v;
+}
+
 int main()
 {
-	string str{"the quick brown fox jumps over the lazy dog"};
-	vector<string> v{explode(str, ' ')};
-	for(auto n:v) cout << n << "\n";
+	vector<string> v;
+	string str;
+
+  {
+    char delimiter = ' ';
+    str = "the quick brown fox jumps over the lazy   dog";
+    cout << '"' << str << '"' << "\n";
+    v = splitChar(str, delimiter, true);
+    for(auto n:v) cout << "ignoring empty fields <" << n << ">\n";
 	
+    str = "the quick brown fox jumps over the lazy   dog";
+    cout << '"' << str << '"' << "\n";
+    v = splitChar(str, delimiter, false);
+    for(auto n:v) cout << "storing empty fields <" << n << ">\n";
+  }
+
+  {
+    string delimiter = string(" ");
+    str = "the quick brown fox jumps over the lazy   dog";
+    cout << '"' << str << '"' << "\n";
+    v = splitString(str, delimiter, true);
+    for(auto n:v) cout << "ignoring empty fields <" << n << ">\n";
+	
+    str = "the quick brown fox jumps over the lazy   dog";
+    cout << '"' << str << '"' << "\n";
+    v = splitString(str, delimiter, false);
+    for(auto n:v) cout << "storing empty fields <" << n << ">\n";
+  }
 	return 0;
 }
