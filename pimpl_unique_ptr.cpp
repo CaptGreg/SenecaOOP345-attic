1c5bcc3d2460d922811899dd9ef13132c0d0394d
CaptGreg <john.blair@senecacollege.ca>
2017-12-04 11:00:21 -0500

    pimpl with unique_ptr

---
+++ pimpl_unique_ptr.cpp
@@ -0,0 +1,73 @@
+// cpp_pimpl.h
+
+#include <memory>
+
+class NetworkManager
+{
+public:
+	NetworkManager();
+	~NetworkManager();
+
+	void Connect();
+	void Disconnect();
+
+private:
+	class NetManImpl;
+	std::unique_ptr<NetManImpl> m_pImpl;
+};
+// cpp_pimpl.cpp
+
+// #include "cpp_pimpl.h"
+#include <iostream>
+
+class NetworkManager::NetManImpl
+{
+public:
+	~NetManImpl()
+	{
+		if (m_connectionOpened)
+			Disconnect();
+	}
+
+	void Connect()
+	{
+		m_connectionOpened = true;
+		std::cout << "NetManImpl::Connect\n";
+	}
+	
+	void Disconnect()
+	{
+		m_connectionOpened = false;
+		std::cout << "NetManImpl::Disconnect\n";
+	}
+	
+private:
+	bool m_connectionOpened { false };
+};
+
+NetworkManager::NetworkManager() : m_pImpl(new NetManImpl()) 
+{
+	
+}
+
+NetworkManager::~NetworkManager() = default;
+
+void NetworkManager::Connect()
+{
+	m_pImpl->Connect();
+}
+
+void NetworkManager::Disconnect()
+{
+	m_pImpl->Disconnect();
+}
+// cpp_pimpl_client.cpp
+
+// #include "cpp_pimpl.h"
+#include <iostream>
+
+int main()
+{
+	NetworkManager man;
+	man.Connect();
+}
