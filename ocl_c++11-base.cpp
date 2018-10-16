4dd3591490b1a5adf6f4f46f7aa6686e83dded78
CaptGreg <john.blair@senecacollege.ca>
2018-10-16 18:07:31 -0400

    fixed for opencl 2.0

--- ocl_c++11-base.cpp
+++ ocl_c++11-base.cpp
@@ -1,5 +1,6 @@
 #define __CL_ENABLE_EXCEPTIONS
-#include <CL/cl.hpp>
+#define CL_HPP_TARGET_OPENCL_VERSION 200
+#include <CL/cl2.hpp>
 
 #include <iostream>
 
@@ -8,7 +9,9 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
   try { 
-    Context(CL_DEVICE_TYPE_DEFAULT);
+    // Context(CL_DEVICE_TYPE_DEFAULT);  // works
+    // Context(CL_DEVICE_TYPE_CPU);      // works
+    Context(CL_DEVICE_TYPE_GPU);         // works
     static const unsigned elements = 20;
     std::vector<float> data(elements, 5);
     Buffer a(begin(data), end(data), true, false);
@@ -25,7 +28,12 @@ int main(int argc, char* argv[]) {
         }
     )d", true);
 
+#if CL_HPP_TARGET_OPENCL_VERSION >= 200
+    auto add = compatibility::make_kernel<Buffer, Buffer, Buffer>(addProg, "add"); // ocl 2.0 comatibility with 1.x
+    // cl2.hpp says "Please use KernelFunctor directly."
+#else
     auto add = make_kernel<Buffer, Buffer, Buffer>(addProg, "add"); // deprecated in ocl 2.0
+#endif
     add(EnqueueArgs(elements), a, b, c);
 
     std::vector<float> result(elements);
