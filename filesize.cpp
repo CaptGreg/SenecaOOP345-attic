b7040235238c93622d6e3e9b509be5304fee95c6
CaptGreg <john.blair@senecacollege.ca>
2019-01-21 12:05:10 -0500

    update to C++ method

--- filesize.cpp
+++ filesize.cpp
@@ -1,17 +1,26 @@
-#include <iostream>
-#include <fstream>
+#include <iostream>                // cout
+#include <fstream>                 // ifstream
+#include <experimental/filesystem> // link with -lstdc++fs library 
+// Verified Jan 2019 with g++ 7.3.0 (Ubuntu 7.3.0-27ubuntu1~18.04)
 
 using namespace std;
 
 int64_t filesize(char*f)
 {
   ifstream i(f, ios::in | ios::binary | ios::ate);
-  return i? uint64_t(i.tellg()): -1;
+  return i.good()? uint64_t(i.tellg()): -1;
 }
 
 int main(int argc, char**argv)
 {
-  for(int arg = 1; arg < argc; arg++)
-    cout << argv[arg] << " = " << filesize(argv[arg]) << "\n";
+  for(int arg = 1; arg < argc; arg++) {
+    cout << "DIY filesize(" << argv[arg] << ") = " << filesize(argv[arg]) << "\n";
+    try {
+        const auto fsize = std::experimental::filesystem::file_size(argv[arg]); 
+        cout <<  "std::filesystem::file_size(" << argv[arg] << ") = " << fsize << "\n";
+    } catch(std::experimental::filesystem::filesystem_error& ex) {
+        std::cout << ex.what() << '\n';
+        // filesystem error: cannot get file size: No such file or directory [xxxyyyxxx]
+    }
+  }
 }
-
