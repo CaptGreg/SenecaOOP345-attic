0f9c511055800df0d52fbbb78bc0ba1c03f57bfd
CaptGreg <john.blair@senecacollege.ca>
2019-04-16 13:31:04 -0400

    c++17 print directory file types

---
+++ dirtree.cpp
@@ -0,0 +1,28 @@
+// https://www.bfilipek.com/2019/04/dir-iterate.html
+// using methods from https://en.cppreference.com/w/cpp/filesystem/is_directory
+
+// g++-8 -Wall -std=c++17  dirtree.cpp -o dirtree -lstdc++fs
+// 2019-04-15 g++-8.2.0
+
+#include <iostream>
+
+// #include <filesystem>            // not found
+#include <experimental/filesystem>
+
+// namespace fs = std::filesystem;  // error: ‘filesystem’ is not a namespace-name
+namespace fs = std::experimental::filesystem;
+
+int main(int argc, char*argv[])
+{
+  const fs::path pathToShow{ argc >= 2 ? argv[1] : fs::current_path() };
+  for (const auto& entry : fs::directory_iterator(pathToShow)) {
+      auto filenameStr = entry.path().filename().string();
+      if ( fs::is_directory(entry.symlink_status()) )  // cached status from directory entry
+        std::cout << "dir:  " << filenameStr << '\n';
+      else
+      if ( fs::is_regular_file(entry.symlink_status()) )  // cached status from directory entry
+        std::cout << "file: " << filenameStr << '\n';
+      else
+          std::cout << "??    " << filenameStr << '\n';
+  }
+}
