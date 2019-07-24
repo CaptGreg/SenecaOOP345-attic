cc69395f94768bfe4d0fbd88d09921b9eb02f338
Olivier Martin <olivier@labapart.com>
2019-07-24 12:07:00 -0400

    run and time the substring code on the complete works of Shakespear -- file gutenberg_shakespeare

--- maxsubstring.cpp
+++ maxsubstring.cpp
@@ -2,30 +2,65 @@
 // random solutions from web, one for length of substring, the other for the substring.
 // not all web solutions work.
 
+#include <algorithm>       // copy_if
+#include <chrono>
+#include <cstring>         // isalpha
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <unordered_map>
 using namespace std;
 
+class Timer {
+  decltype(std::chrono::high_resolution_clock::now()) start, stop;
+public:
+  void Start() { start = std::chrono::high_resolution_clock::now(); }
+  void Stop()  { stop  = std::chrono::high_resolution_clock::now(); }
+  uint64_t millisecs() { return std::chrono::duration_cast<std::chrono::duration<int,std::milli> >(stop-start).count(); }
+  uint64_t microsecs() { return std::chrono::duration_cast<std::chrono::duration<int,std::micro> >(stop-start).count(); }
+  uint64_t nanosecs () { return std::chrono::duration_cast<std::chrono::duration<int,std::nano>  >(stop-start).count(); }
+};
+
 // https://duckduckgo.com/?q=c%2B%2B+find+the+longest+sub-string+wothout+a+repeating+character&t=canonical&atb=v1-1&ia=web&iax=qa
-int lengthOfLongestSubstring(string& s) {
+int lengthOfLongestSubstring(string& s) { // GB fast
     size_t i = 0, max = 0;
     int offset = -1;
     int arr[256];
     fill_n(arr, 256, -1);
     for (i = 0; i < s.length(); i++) {
         int si = (int) s[i];
-        offset = std::max(offset,arr[si]);
+        offset = std::max(offset,arr[si]); // -1, unless seen before
         max = std::max(max,i-offset);
         arr[si] = i;
     } 
+
     return max;
+}
+
+string findMaxSubstring(string& s) { // GB adapt lengthOfLongestSubstringa to return substring
+    size_t i = 0, max = 0;
+    int offset = -1;
+    size_t pos = 0;   // GB
+    int arr[256];
+    fill_n(arr, 256, -1);
+    for (i = 0; i < s.length(); i++) {
+        int si = (int) s[i];
+        offset = std::max(offset,arr[si]); // -1, unless seen before
+        // max = std::max(max,i-offset);   // GB save position of max string
+        if(i-offset > max) {
+          max = i-offset;
+          pos = i;                    // GB
+        }
+        arr[si] = i;
+    } 
+
+    return max? s.substr(pos+1-max, max): "";
 }
 
 // https://www.geeksforgeeks.org/print-longest-substring-without-repeating-characters/
 // Function to find and print longest 
 // substring without repeating characters. 
-string findLongestSubstring(string& str) 
+string findLongestSubstring(string& str)   // GB slow
 { 
     int i; 
     int n = str.length(); 
@@ -100,8 +135,11 @@ string findLongestSubstring(string& str)
 
 int main()
 {
+  int r = EXIT_SUCCESS;
+
+  // string s = "oandamakesmoneyfromcurrencyexchange";
   string s = "oandamakesmoney";
-  cout << "'" << s << "' = (" << lengthOfLongestSubstring(s) << ") '" << findLongestSubstring(s) << "'\n";
+  cout << "'" << s << "' = (" << lengthOfLongestSubstring(s) << ") '" << "\n"; // findLongestSubstring(s) << "'\n";
 
   // test data from https://stackoverflow.com/questions/9734474/find-longest-substring-without-repeating-characters
   string input[] = { { "" }, { "a" }, { "ab" }, { "aab" }, { "abb" },
@@ -110,7 +148,48 @@ int main()
             { "abcdeafghicabcdefghijklmnopqrabcdx" },
             { "zxxaabcdeafghicabcdefghijklmnopqrabcdx" },
             {"aaabcdefgaaa"}};
-  for (auto s : input) {
+  for (auto s : input)
     cout << "'" << s << "' = (" << lengthOfLongestSubstring(s) << ") '" << findLongestSubstring(s) << "'\n";
+
+  string file = "gutenberg_shakespeare";
+  cout << "\n" << file << " example\n";
+  fstream fs(file, ios::in);
+  if(fs.is_open()) {
+    string data { istreambuf_iterator<char>(fs), istreambuf_iterator<char>() }; // read file
+    // string data;
+    // copy(istreambuf_iterator<char>(fs), istreambuf_iterator<char>(), data.begin()); // read file
+    fs.close();
+    cout << data.size() << " chars read\n";
+    string s;
+    // s.reserve(data.size());
+    // copy_if(data.begin(), data.end(), s.begin(), ::isalpha);
+    // copy_if(data.begin(), data.begin() + 10000, s.begin(), ::isalpha);
+    // cout << "copy_if complete\n";
+    for(auto e : data) if(::isalpha(e)) s += e;
+    cout << s.size() << " alpha chars\n";
+
+    Timer t;
+    t.Start();
+    int length = lengthOfLongestSubstring(s);
+    t.Stop();
+    cout << "time for length: " << length << " " << 1e-9 * t.nanosecs() << " s.\n";
+
+    t.Start();
+    string result = findLongestSubstring(s);
+    t.Stop();
+    cout << "time for findLongestSubstring: '" << result << "' " << 1e-9 * t.nanosecs() << " s.\n";
+
+    t.Start();
+    result = findMaxSubstring(s);
+    t.Stop();
+    cout << "time for findMaxSubstring: '" << result << "' " << 1e-9 * t.nanosecs() << " s.\n";
+
+
+    cout << "ALL DONE.\n";
+  } else {
+    cerr << "cannot open '" << file << "'\n";
+    r = EXIT_FAILURE;
   }
+
+  return r;
 }
