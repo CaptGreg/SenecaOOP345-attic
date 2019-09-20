e09ca254e0b883b8983d824c510fb727ff315957
Olivier Martin <olivier@labapart.com>
2019-09-20 17:09:56 -0400

    minor clean up of thread-pipeline.cpp

--- thread-pipeline.cpp
+++ thread-pipeline.cpp
@@ -2,46 +2,39 @@
 #include <mutex>
 #include <condition_variable>
 #include <iostream>
-#include <string>  // use string for lock-free writing to cout
+#include <string>
 #include <cstdlib> // rand
-#include <chrono>  // clocks
-
-using namespace std;
+#include <chrono>
 
 class Barrier { // http://stackoverflow.com/questions/24465533/implementing-boostbarrier-in-c11
-  std::mutex m_mutex;
-  std::condition_variable m_cv;
+  std::mutex              m;
+  std::condition_variable cv;
 
-  size_t m_count;
-  const size_t m_initial;
+  size_t                  count;
+  const size_t            initial;
 
-  enum State : unsigned char { Up, Down };
-  State m_state;
+  enum State : uint8_t    { Up, Down };
+  State                   state;
 public:
-  explicit Barrier(std::size_t count) 
-    : m_count{ count }, 
-      m_initial{ count }, 
-      m_state{ State::Down }
-    { }
+  explicit Barrier(std::size_t count) : count{ count }, initial{ count }, state{ State::Down } { }
 
-  /// Blocks until all N threads reach here
-  void wait()
+  void wait() /// Blocks until all N threads call wait
   {
-    std::unique_lock<std::mutex> lock{ m_mutex };
+    std::unique_lock<std::mutex> lock{ m };
 
-    if (m_state == State::Down) { // Counting down the number of waiting threads
-      if (--m_count == 0) {
-        m_state = State::Up;
-        m_cv.notify_all();
+    if (state == State::Down) { // Counting down the number of waiting threads
+      if (--count == 0) {
+        state = State::Up;
+        cv.notify_all();
       } else {
-        m_cv.wait(lock, [this] { return m_state == State::Up; });
+        cv.wait(lock, [this] { return state == State::Up; });
       }
-    } else { // (m_state == State::Up) // Counting back up for auto reset
-      if (++m_count == m_initial) {
-        m_state = State::Down;
-        m_cv.notify_all();
+    } else { // (state == State::Up) // Counting back up for auto reset
+      if (++count == initial) {
+        state = State::Down;
+        cv.notify_all();
       } else {
-        m_cv.wait(lock, [this] { return m_state == State::Down; });
+        cv.wait(lock, [this] { return state == State::Down; });
       }
     }
   }
@@ -50,54 +43,57 @@ public:
 const int NUM_THREADS = 5;
 Barrier barrier(NUM_THREADS+1); // +1 for main
 
-std::chrono::time_point<std::chrono::high_resolution_clock> start;
-auto Time = [start] () -> string { // syntax error without ()
-  std::chrono::time_point<std::chrono::high_resolution_clock> now = std::chrono::high_resolution_clock::now();
+auto Time = [] {
+  auto now = std::chrono::high_resolution_clock::now();
+  static bool initialized = false;
+  static decltype(now) epoch;
+  if(not initialized) {
+    epoch = std::chrono::high_resolution_clock::now();
+    initialized = true;
+  }
+
   typedef std::chrono::duration<int,std::micro> microsecs_t; 
-  microsecs_t duration_get( std::chrono::duration_cast<microsecs_t>(now-start) ) ;
-  auto t = duration_get.count();
-  return to_string( t ) + " ";
+  microsecs_t duration_get( std::chrono::duration_cast<microsecs_t>(now-epoch) ) ;
+  auto t = 1e-6*duration_get.count();
+
+  return std::to_string( t ) + " ";
 };
 
-
-void tFun(int tid, int loopCount)
+void tFun(int threadNum, int loopCount)
 {
-  // auto tid = this_thread::get_id();
+  // auto threadNum = this_thread::get_id();
   auto nap = 1000 + rand() % 2000;
-  cout << Time() + string("thread ")  + to_string(tid) + " alive.\n";
+  std::cout << Time() + std::string("thread ")  + std::to_string(threadNum) + " alive.\n";
 
-  this_thread::sleep_for(chrono::milliseconds(nap));
+  std::this_thread::sleep_for(std::chrono::milliseconds(nap));
 
+  std::string ID(std::string("thread ") + std::to_string(threadNum));
   for(int count = 0; count < loopCount; count++) {
-    cout << Time() + string("thread ")  + to_string(tid) + " count=" + to_string(count) + " at barrier.\n";
+    std::cout << Time() + ID + " count=" + std::to_string(count) + " at barrier.\n";
     barrier.wait();
-    cout << Time() + string("thread ")  + to_string(tid) + " passed barrier.\n";
-    this_thread::sleep_for(chrono::milliseconds(nap));
+    std::cout << Time() + ID + " passed barrier.\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(nap));
   }
   barrier.wait(); // everyone exit at once
-  cout << Time() + string("thread ")  + to_string(tid) + " exiting.\n";
-
+  std::cout << Time() + ID + " exiting.\n";
 }
 
 int main(int argc, char*argv[])
 {
-  start = std::chrono::high_resolution_clock::now();  // set start time
-
-  thread tid[NUM_THREADS];
+  std::thread tid[NUM_THREADS];
   const int loopCount = 4;
 
   for(int t = 0; t < NUM_THREADS; t++) {
-    tid[t] = thread(tFun, t, loopCount);
+    tid[t] = std::thread(tFun, t, loopCount);
     // thread(tFun, t, loopCount).detach();
   }
 
   for(int count = 0; count < loopCount; count++) {
-    cout << Time() + "main waiting at barrier.\n";
+    std::cout << Time() + "main waiting at barrier.\n";
     barrier.wait();
   }
 
   barrier.wait();  // wait for threads to exit
   for(auto &e : tid) e.join();
-  cout << Time() + " main exiting.\n";
-
+  std::cout << Time() + " main exiting.\n";
 }
