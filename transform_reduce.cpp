02ff0b6b4a596872d915e17f06095b061d654293
Olivier Martin <olivier@labapart.com>
2019-09-20 17:12:18 -0400

    finally, transform_reduce support with g++9.1 or later, clang++-8

--- transform_reduce.cpp
+++ transform_reduce.cpp
@@ -1,65 +1,51 @@
-// https://en.cppreference.com/w/cpp/algorithm/transform_reduce
-// Equivalent to 
-//  transform_reduce(first1, last1, first2, init, std::plus<>(), std::multiplies<>());
-// effectively parallelized version of the default std::inner_product
+// algorithm: sequential inner_product vs parallel transform_reduce (g++9.1/clang++-8 or later with -ltbb)
 
-// http://en.cppreference.com/w/cpp/algorithm/transform_reduce
-
-// g++-8 -Wall -std=c++17   transform_reduce.cpp -o transform_reduce -pthread
-// g++-8 -Wall -std=c++1y   transform_reduce.cpp -o transform_reduce -pthread
-// g++-8 -Wall -std=c++1z   transform_reduce.cpp -o transform_reduce -pthread
-// g++-8 -Wall -std=gnu++17 transform_reduce.cpp -o transform_reduce -pthread
-// g++-8 -Wall -std=gnu++2a transform_reduce.cpp -o transform_reduce -pthread
-
-#include <vector>
-#include <functional>
+#include <algorithm> 
+#include <chrono>
+#include <execution>
 #include <iostream>
-#include <numeric>
-// #include <execution> // error: execution: No such file or directory
+#include <vector>
 
-#include <parallel/parallel.h> 
-// error: ‘std::execution’ has not been declared std::execution::par,
-//  reduce.cpp:47:30: error: ‘reduce’ is not a member of ‘std’
-//          double result = std::reduce(std::execution::par, v.begin(), v.end());
-//                               ^~~~~~
-// reduce.cpp:47:42: error: ‘std::execution’ has not been declared
-//          double result = std::reduce(std::execution::par, v.begin(), v.end());
-
-#include <algorithm> // http://en.cppreference.com/w/cpp/header/execution says <execution> part of <algorithm>
-// #include <algorithm/execution> // error: algorithm/execution: No such file or directory
-
-// error: ‘reduce’ is not a member of ‘std’
-//          double result = std::reduce(std::execution::par, v.begin(), v.end());
-//                               ^~~~~~
-// reduce.cpp:55:30: note: suggested alternative: ‘replace’
-//          double result = std::reduce(std::execution::par, v.begin(), v.end());
-//                               ^~~~~~
-//                               replace
-// reduce.cpp:55:42: error: ‘std::execution’ has not been declared
-//          double result = std::reduce(std::execution::par, v.begin(), v.end());
-
-
-// $ find /usr/include  -name execution
-// (nothing found)
-
-// https://en.cppreference.com/w/cpp/algorithm/transform_reduce
 int main()
 {
-  int size = 3;
-  std::vector<double> X(size, 1.0), Y(size, 1.0);
+  class Timer {
+    decltype(std::chrono::high_resolution_clock::now()) start, stop;
+  public:
+    void Start() { start = std::chrono::high_resolution_clock::now(); }
+    void Stop()  { stop  = std::chrono::high_resolution_clock::now(); }
+    uint64_t nanosecs () { return std::chrono::duration_cast<std::chrono::duration<int,std::nano>  >(stop-start).count(); }
+  } t;
+  float nsInner_product;
+  float nsTransform_reduce;
 
-  std::transform(X.begin(), X.end(), X.begin(), [] (double v) ->double {static double c=0; return c++;}); // GB
-  std::transform(Y.begin(), Y.end(), Y.begin(), [] (double v) ->double {static double c=0; return c++;}); // GB
+  const int SIZE = 1'000'000;
+  std::vector<double> X(SIZE, 1.0), Y(SIZE, 0.5);
 
-  // double result = std::transform_reduce // error: ‘transform_reduce’ is not a member of ‘std’
-  double result = std::inner_product // GB
-  (
-    // std::execution::par,       // GB someday...
-    X.begin(), X.end(),
-    Y.begin(), 
-    0.0
-  );
-  std::cout << result << '\n';
+  t.Start();
+  double result = std::inner_product ( X.begin(), X.end(), Y.begin(), 0.0);
+  t.Stop();
+  nsInner_product = t.nanosecs();
+  std::cout << "sequential inner_product    " << result << " in " << 1e-3*nsInner_product << " us.\n";
 
-  return 0;
+  t.Start();
+  result = transform_reduce( std::execution::seq, X.begin(), X.end(), Y.begin(), 0.0 );
+  t.Stop();
+  nsTransform_reduce = t.nanosecs();
+  std::cout << "sequential transform_reduce " << result << " in " << 1e-3*nsTransform_reduce << " us.\n";
+
+  t.Start();
+  result = transform_reduce( std::execution::par, X.begin(), X.end(), Y.begin(), 0.0 );
+  t.Stop();
+  nsTransform_reduce = t.nanosecs();
+  std::cout << "parallel   transform_reduce " << result << " in " << 1e-3*nsTransform_reduce << " us.\n";
+
+  t.Start();
+  result = transform_reduce( std::execution::par, X.begin(), X.end(), Y.begin(), 0.0 );
+  t.Stop();
+  nsTransform_reduce = t.nanosecs();
+  std::cout << "parallel   transform_reduce " << result << " in " << 1e-3*nsTransform_reduce << " us.\n";
+
+  std::cout << "once the thread pool starts up, parallel transform_reduce is about " << (0.+nsInner_product)/(0.+nsTransform_reduce)  << " times faster than sequential inner_product\n";
+
+  return EXIT_SUCCESS;
 }
