66c8840b961b6c5b2762e22ea9feea67ea189aa4
Olivier Martin <olivier@labapart.com>
2020-03-22 10:05:13 -0400

    add #pragma date, time message

--- compiler.cpp
+++ compiler.cpp
@@ -2,16 +2,18 @@
 
 int main(int , char* argv[])
 {
+  #pragma message "Compiling " __FILE__  " at : " __TIME__ " on " __DATE__
+
   std::cout <<"+++++++++++++++++++++++++++++++\n";
   std::cout << argv[0] << "\n";
-  std::cout << "FILE '" << __FILE__ << "' compiled " << __DATE__ << " at " << __TIME__ 
-#ifdef __GNUC__  // either g++ or clang++
-  #ifdef __clang__
-    << " by clang++ "
-  #else
-    << " by g++ "
-  #endif
-   << __VERSION__
-#endif
-   << "\n\n";
+  std::cout << "FILE '" __FILE__ "' compiled " __DATE__ " at " __TIME__  " on " __DATE__ 
+    #ifdef __GNUC__  // either g++ or clang++
+      #ifdef __clang__
+        " by clang++ "
+      #else
+        " by g++ "
+      #endif
+       __VERSION__
+    #endif
+       "\n\n";
 }
