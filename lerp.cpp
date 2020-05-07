c79c6752d8f02f9584ac6c0a2c7e1d7065deaae1
Olivier Martin <olivier@labapart.com>
2020-05-07 15:28:46 -0400

    add lerp example

---
+++ lerp.cpp
@@ -0,0 +1,14 @@
+// https://en.cppreference.com/w/cpp/numeric/lerp
+
+#include <iostream>
+#include <cmath>
+ 
+int main()
+{
+    float a=10.0f, b=20.0f;
+ 
+    std::cout << "a=" << a << '\n'
+              << "b=" << b << '\n'
+              << "mid point=" << std::lerp(a,b,0.5f) << '\n'
+              << std::boolalpha << (a == std::lerp(a,b,0.0f)) << '\n';
+}
