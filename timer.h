44f6caf01413d1c9f52c2c6de1569ec6ea4624f8
Olivier Martin <olivier@labapart.com>
2020-10-29 13:27:13 -0400

    add sleet functions to timer class

--- timer.h
+++ timer.h
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <iostream>
 
+#if 0
 class Timer {
   decltype(std::chrono::high_resolution_clock::now()) now, start, stop;
 public:
@@ -19,5 +20,27 @@ public:
   uint64_t millisecs() { return std::chrono::duration_cast<std::chrono::duration<int,std::milli> >(stop-start).count(); }
   uint64_t microsecs() { return std::chrono::duration_cast<std::chrono::duration<int,std::micro> >(stop-start).count(); }
   uint64_t nanosecs () { return std::chrono::duration_cast<std::chrono::duration<int,std::nano>  >(stop-start).count(); }
+};
+#endif
+
+class Timer {
+  using hrc = std::chrono::high_resolution_clock;
+  using sc  = std::chrono::system_clock;
+  decltype(hrc::now()) start, stop;
+public:
+  void PrintNow()   { 
+      auto now = sc::now();
+      auto tt  = sc::to_time_t(now);
+      std::cout << std::put_time(std::localtime(&tt), "%T ");
+  }
+  void Start() { start = hrc::now(); }
+  void Stop()  { stop  = hrc::now(); }
+  uint64_t millisecs() { return std::chrono::duration_cast<std::chrono::duration<int,std::milli> >(stop-start).count(); }
+  uint64_t microsecs() { return std::chrono::duration_cast<std::chrono::duration<int,std::micro> >(stop-start).count(); }
+  uint64_t nanosecs () { return std::chrono::duration_cast<std::chrono::duration<int,std::nano>  >(stop-start).count(); }
+  void  sleep(int s)  { std::this_thread::sleep_for(std::chrono::seconds(s)); } 
+  void msleep(int ms) { std::this_thread::sleep_for(std::chrono::milliseconds(ms)); }
+  void usleep(int us) { std::this_thread::sleep_for(std::chrono::microseconds(us)); }
+  void nsleep(int ns) { std::this_thread::sleep_for(std::chrono::nanoseconds(ns)); }
 };
 
